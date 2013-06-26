2f933bfafd611cd485c8316cc069ba7d4aa2d279
abbajoa <abbajoa@gmail.com>
2013-06-26 23:07:10 +0900

    add point ds, some util classes

---
+++ src/main/java/org/psjava/geometry/data/PointByXComparator.java
@@ -0,0 +1,15 @@
+package org.psjava.geometry.data;
+
+import java.util.Comparator;
+
+public class PointByXComparator {
+
+	public static <T> Comparator<Point2D<T>> create(final Comparator<T> comaprator) {
+		return new Comparator<Point2D<T>>() {
+			@Override
+			public int compare(Point2D<T> p1, Point2D<T> p2) {
+				return comaprator.compare(p1.x(), p2.x());
+			}
+		};
+	}
+}
---
+++ src/main/java/org/psjava/geometry/data/PointByYComparator.java
@@ -0,0 +1,15 @@
+package org.psjava.geometry.data;
+
+import java.util.Comparator;
+
+public class PointByYComparator {
+
+	public static <T> Comparator<Point2D<T>> create(final Comparator<T> comaprator) {
+		return new Comparator<Point2D<T>>() {
+			@Override
+			public int compare(Point2D<T> p1, Point2D<T> p2) {
+				return comaprator.compare(p1.y(), p2.y());
+			}
+		};
+	}
+}
