5b4273cab5ac57ba145b1b381e47abe0a55120ca
abbajoa <abbajoa@gmail.com>
2013-06-28 14:19:28 +0900

    add util class

---
+++ src/main/java/org/psjava/math/InRange.java
@@ -0,0 +1,11 @@
+package org.psjava.math;
+
+import java.util.Comparator;
+
+public class InRange {
+	
+	public static <T> boolean is(T v, T lower, T upper, Comparator<T> comp) {
+		return comp.compare(lower, v) <= 0 && comp.compare(v, upper) <= 0;
+	}
+
+}
