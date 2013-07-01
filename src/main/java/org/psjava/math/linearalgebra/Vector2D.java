a540775726cf32356c702640ef6278e9036e39fa
abbajoa <abbajoa@gmail.com>
2013-07-02 00:57:19 +0900

    add data type

---
+++ src/main/java/org/psjava/math/linearalgebra/Vector2D.java
@@ -0,0 +1,22 @@
+package org.psjava.math.linearalgebra;
+
+public class Vector2D<T> {
+	private final T x, y;
+
+	public static <T> Vector2D<T> create(T x, T y) {
+		return new Vector2D<T>(x, y);
+	}
+
+	private Vector2D(T x, T y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public T x() {
+		return x;
+	}
+
+	public T y() {
+		return y;
+	}
+}
