b386c28a25adda26d15d38dd25fbb8d89275e5a3
abbajoa <abbajoa@gmail.com>
2013-07-02 00:52:37 +0900

    add some geometry data types

--- src/main/java/org/psjava/ds/array/ArrayUtil.java
+++ src/main/java/org/psjava/ds/array/ArrayUtil.java
@@ -3,11 +3,12 @@ package org.psjava.ds.array;
 
 public class ArrayUtil {
 
-	public static <T> T getLast(Array<T> a) {
+	public static <T> T getLast(Array<T> a) { // TODO move to LastInArray class
 		return a.get(a.size()-1);
 	}
 
-	public static <T> T getFirst(Array<T> a) {
+	public static <T> T getFirst(Array<T> a) { // TODO move to FirstInArray
+												// class
 		return a.get(0);
 	}
 
