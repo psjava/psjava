f1e0d2e63889d912554df13e5a13a3ebbbfc271a
abbajoa <abbajoa@gmail.com>
2013-07-07 23:22:22 +0900

    add sub array class

---
+++ src/main/java/org/psjava/ds/array/SubArray.java
@@ -0,0 +1,38 @@
+package org.psjava.ds.array;
+
+import java.util.Iterator;
+
+import org.psjava.javautil.IterableToString;
+
+public class SubArray {
+
+	public static <T> Array<T> create(final Array<T> original, final int start, final int end) {
+		return new Array<T>() {
+			@Override
+			public T get(int index) {
+				return original.get(start + index);
+			}
+
+			@Override
+			public int size() {
+				return end - start;
+			}
+
+			@Override
+			public final boolean isEmpty() {
+				return size() == 0;
+			}
+
+			@Override
+			public final Iterator<T> iterator() {
+				return ArrayIterator.create(this);
+			}
+
+			@Override
+			public final String toString() {
+				return IterableToString.toString(this);
+			}
+		};
+	}
+
+}
