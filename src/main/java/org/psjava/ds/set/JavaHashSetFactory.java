2a2c39cc9d952fa0c87c2706a2a1d3f2f5f94b96
abbajoa <abbajoa@gmail.com>
2013-07-08 11:26:11 +0900

    introduce goods for set factory

--- src/main/java/org/psjava/ds/set/JavaHashSetFactory.java
+++ src/main/java/org/psjava/ds/set/JavaHashSetFactory.java
@@ -3,9 +3,15 @@ package org.psjava.ds.set;
 import java.util.HashSet;
 
 
-public class JavaHashSetFactory implements MutableSetFactory {
-	@Override
-	public <T> MutableSet<T> create() {
-		return new MutableSetUsingJavaSet<T>(new HashSet<T>());
+public class JavaHashSetFactory {
+
+	public static MutableSetFactory getInstance() {
+		return new MutableSetFactory() {
+			@Override
+			public <T> MutableSet<T> create() {
+				return new MutableSetUsingJavaSet<T>(new HashSet<T>());
+			}
+		};
 	}
+
 }
---
+++ src/main/java/org/psjava/goods/GoodMutableSetFactory.java
@@ -0,0 +1,10 @@
+package org.psjava.goods;
+
+import org.psjava.ds.set.JavaHashSetFactory;
+import org.psjava.ds.set.MutableSetFactory;
+
+public class GoodMutableSetFactory {
+	public static MutableSetFactory getInstance() {
+		return JavaHashSetFactory.getInstance();
+	}
+}
