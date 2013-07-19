d3f10f8af9da9c8a430fb41d49de70dfd6386761
abbajoa <abbajoa@gmail.com>
2013-07-20 02:10:07 +0900

    fix test fail

--- src/test/java/org/psjava/algo/search/BinarySearchExample.java
+++ src/test/java/org/psjava/algo/search/BinarySearchExample.java
@@ -1,26 +1,28 @@
 package org.psjava.algo.search;
 
+import java.util.Comparator;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
 import org.psjava.ds.array.MutableArrayUsingIntArray;
 import org.psjava.javautil.DefaultComparator;
+import org.psjava.javautil.ReversedComparator;
 
 public class BinarySearchExample {
 
-	// TODO complemet cases
-
 	@Test
 	public void searchInNormalArray() {
-		int[] a = { 1, 3, 5, 7, 9 };
-		int res = BinarySearchFirstInArray.search(MutableArrayUsingIntArray.wrap(a), 5, new DefaultComparator<Integer>(), -1);
+		int[] array = new int[] { 1, 3, 5, 7, 9 };
+		int res = BinarySearchFirstInArray.search(MutableArrayUsingIntArray.wrap(array), 5, new DefaultComparator<Integer>(), -1);
 		Assert.assertEquals(2, res);
 	}
 
 	@Test
 	public void searchInReversedArray() {
-		int[] a = { 9, 7, 5, 3, 1 };
-		int res = BinarySearchFirstInArray.search(MutableArrayUsingIntArray.wrap(a), 3, new DefaultComparator<Integer>(), -1);
+		int[] array = new int[] { 9, 7, 5, 3, 1 };
+		Comparator<Integer> comp = ReversedComparator.wrap(new DefaultComparator<Integer>());
+		int res = BinarySearchFirstInArray.search(MutableArrayUsingIntArray.wrap(array), 3, comp, -1);
 		Assert.assertEquals(3, res);
 	}
 
