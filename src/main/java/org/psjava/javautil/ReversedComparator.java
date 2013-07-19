3c67ec6128729ccc2ad277be258fd12fe2661ff2
abbajoa <abbajoa@gmail.com>
2013-07-20 02:09:36 +0900

    add util class

---
+++ src/main/java/org/psjava/javautil/ReversedComparator.java
@@ -0,0 +1,16 @@
+package org.psjava.javautil;
+
+import java.util.Comparator;
+
+public class ReversedComparator {
+	
+	public static <T> Comparator<T> wrap(final Comparator<T> original) {
+		return new Comparator<T>() {
+			@Override
+			public int compare(T o1, T o2) {
+				return original.compare(o2, o1);
+			}
+		};
+	}
+
+}
