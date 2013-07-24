f935a8ed79a91560fa48c3031a70a9e61853926e
abbajoa <abbajoa@gmail.com>
2013-07-25 01:41:27 +0900

    add new binary search core

---
+++ src/main/java/org/psjava/algo/search/BinarySearchFirst.java
@@ -0,0 +1,32 @@
+package org.psjava.algo.search;
+
+import java.util.Comparator;
+
+import org.psjava.math.calc.FloorDivide;
+import org.psjava.math.ns.IntegerDivisableNumberSystem;
+
+public class BinarySearchFirst {
+
+	public static <I, O> I search(IntegerDivisableNumberSystem<I> inputNumberSystem, Function<I, O> f, Comparator<O> sortedOrder, I begin, I end, O target, I def) {
+		I r = searchFirstPositionCanBeInserted(inputNumberSystem, begin, end, target, f, sortedOrder);
+		if (!r.equals(end) && f.get(r).equals(target))
+			return r;
+		else
+			return def;
+	}
+
+	private static <I, O> I searchFirstPositionCanBeInserted(IntegerDivisableNumberSystem<I> ns, I begin, I end, O value, Function<I, O> f, Comparator<O> sortedOrder) {
+		I one = ns.getOne();
+		I two = ns.add(one, one);
+		while (ns.compare(begin, end) < 0) {
+			I midKey = FloorDivide.calc(ns, ns.add(begin, end), two);
+			O midValue = f.get(midKey);
+			if (sortedOrder.compare(midValue, value) < 0)
+				begin = ns.add(midKey, one);
+			else
+				end = midKey;
+		}
+		return end;
+	}
+
+}
---
+++ src/test/java/org/psjava/algo/search/BinarySearchLastTest.java
@@ -0,0 +1,25 @@
+package org.psjava.algo.search;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.psjava.javautil.DefaultComparator;
+import org.psjava.math.ns.IntegerNumberSystem;
+
+public class BinarySearchLastTest {
+
+	private static final IntegerNumberSystem NS = IntegerNumberSystem.getInstance();
+	private static final DefaultComparator<Integer> COMP = new DefaultComparator<Integer>();
+
+	@Test
+	public void test() {
+		Function<Integer, Integer> f = new Function<Integer, Integer>() {
+			@Override
+			public Integer get(Integer input) {
+				return input / 2;
+			}
+		};
+		Assert.assertEquals(3, (int) BinarySearchLast.search(NS, f, COMP, 0, 10, 1, -1));
+		Assert.assertEquals(-1, (int) BinarySearchLast.search(NS, f, COMP, 0, 10, 99, -1));
+	}
+
+}
