fea9bc72f2e3f382864877b88be9eabd01578058
abbajoa <abbajoa@gmail.com>
2013-07-26 16:20:29 +0900

    apply the simple test util

--- src/test/java/org/psjava/algo/sort/RandomizedQuickSortTest.java
+++ src/test/java/org/psjava/algo/sort/RandomizedQuickSortTest.java
@@ -1,17 +1,14 @@
 package org.psjava.algo.sort;
 
 import org.junit.Test;
-import org.psjava.ds.array.MutableArray;
-import org.psjava.ds.array.MutableArrayFromValues;
-import org.psjava.javautil.DefaultComparator;
 
 
 public class RandomizedQuickSortTest {
 
 	@Test
 	public void test() {
-		MutableArray<Integer> a = MutableArrayFromValues.create(3, 1, 2);
-		RandomizedQuickSort.getInstance().sort(a, new DefaultComparator<Integer>());
+		Sort sort = RandomizedQuickSort.getInstance();
+		SortTestUtil.testSimpleSort(sort);
 	}
 
 }
