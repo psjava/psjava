4da3045da68439d35d02ba478d1afdb69df68607
abbajoa <abbajoa@gmail.com>
2013-08-11 18:05:39 +0900

    fix -1/2 case bug

--- src/test/java/org/psjava/math/calc/FloorDivideTest.java
+++ src/test/java/org/psjava/math/calc/FloorDivideTest.java
@@ -15,5 +15,7 @@ public class FloorDivideTest {
 		Assert.assertEquals(1, (int) FloorDivide.calc(NS, -3, -2));
 		Assert.assertEquals(-2, (int) FloorDivide.calc(NS, -3, 2));
 		Assert.assertEquals(-2, (int) FloorDivide.calc(NS, 3, -2));
+		Assert.assertEquals(-1, (int) FloorDivide.calc(NS, -1, 2));
+		Assert.assertEquals(0, (int) FloorDivide.calc(NS, 1, 2));
 	}
 }
