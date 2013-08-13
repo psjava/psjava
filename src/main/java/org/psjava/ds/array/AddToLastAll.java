963d8ae6ec1ed0215b0d850393026d730b273128
abbajoa <abbajoa@gmail.com>
2013-08-13 10:38:14 +0900

    refactor-rename

--- src/main/java/org/psjava/ds/array/AddToLastAll.java
+++ src/main/java/org/psjava/ds/array/AddToLastAll.java
@@ -2,9 +2,9 @@ package org.psjava.ds.array;
 
 public class AddToLastAll {
 
-	public static <T> void add(DynamicArray<T> dynamicArray, Iterable<? extends T> values) {
+	public static <T> void add(DynamicArray<T> target, Iterable<? extends T> values) {
 		for (T v : values)
-			dynamicArray.addToLast(v);
+			target.addToLast(v);
 	}
 
 }
