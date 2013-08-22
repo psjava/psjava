e26f07fee0c81d043a36b83477e75fc964a04888
kongbomb <kongbomb.coder@gmail.com>
2013-08-22 18:11:41 +0900

    add a good one : shortest path

---
+++ src/main/java/org/psjava/goods/GoodSingleSourceShortestPath.java
@@ -0,0 +1,12 @@
+package org.psjava.goods;
+
+import org.psjava.algo.graph.shortestpath.Dijkstra;
+import org.psjava.algo.graph.shortestpath.SingleSourceShortestPath;
+import org.psjava.ds.heap.BinaryHeapFactory;
+
+public class GoodSingleSourceShortestPath {
+
+	public static SingleSourceShortestPath getInstance() {
+		return new Dijkstra(new BinaryHeapFactory());
+	}
+}
