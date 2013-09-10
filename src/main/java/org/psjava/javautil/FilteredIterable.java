b2a3cf9ca044ce6da085ff4015a866a4731036fd
kongbomb <kongbomb.coder@gmail.com>
2013-09-11 01:35:13 +0900

    extract a class from FilteredIterable

--- src/main/java/org/psjava/javautil/FilteredIterable.java
+++ src/main/java/org/psjava/javautil/FilteredIterable.java
@@ -2,53 +2,15 @@ package org.psjava.javautil;
 
 import java.util.Iterator;
 
-
-
 public class FilteredIterable {
-	
+
 	public static <T> Iterable<T> create(final Iterable<? extends T> original, final DataFilter<T> filter) {
 		return new Iterable<T>() {
+			@Override
 			public Iterator<T> iterator() {
-				return new FilteredIterator<T>(filter, original);
+				return FilteredIterator.create(original.iterator(), filter);
 			}
 		};
-	}
-
-	private static final class FilteredIterator<T> extends ReadOnlyIterator<T> {
-		private final DataFilter<T> filter;
-		private final Iterator<? extends T> cursor;
-		private T next = null;
-
-		private FilteredIterator(DataFilter<T> filter, Iterable<? extends T> original) {
-			this.filter = filter;
-			cursor = original.iterator();
-		}
-
-		@Override
-		public boolean hasNext() {
-			tryToStepNext();
-			return next != null;
-		}
-
-		@Override
-		public T next() {
-			tryToStepNext();
-			T r = next;
-			next = null;
-			return r;
-		}
-
-		private void tryToStepNext() {
-			if (next == null) {
-				while(cursor.hasNext()) {
-					T value = cursor.next();
-					if(filter.isAccepted(value)) {
-						next = value;
-						break;
-					}
-				}
-			}
-		}
 	}
 
 	private FilteredIterable() {
