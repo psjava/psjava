1a155fd5743cdbaea2e7c3dea49d6cb80b984792
kongbomb <kongbomb.coder@gmail.com>
2013-09-24 22:25:58 +0900

    relax interface

--- src/main/java/org/psjava/ds/set/InsertAllToSet.java
+++ src/main/java/org/psjava/ds/set/InsertAllToSet.java
@@ -2,7 +2,7 @@ package org.psjava.ds.set;
 
 
 public class InsertAllToSet {
-	public static <T> void insertAll(MutableSet<T> set, Iterable<T> values) {
+	public static <T> void insertAll(MutableSet<T> set, Iterable<? extends T> values) {
 		for(T v : values) 
 			set.insert(v);
 	}
