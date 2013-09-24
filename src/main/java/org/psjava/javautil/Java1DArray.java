f1f3a983bfb4747b8d744dc5487b4535669b5b50
kongbomb <kongbomb.coder@gmail.com>
2013-09-24 22:31:53 +0900

    refactoring-rename

--- src/main/java/org/psjava/javautil/Java1DArray.java
+++ src/main/java/org/psjava/javautil/Java1DArray.java
@@ -3,8 +3,8 @@ package org.psjava.javautil;
 public class Java1DArray {
 
 	@SuppressWarnings("unchecked")
-	public static <W> W[] create(Class<?> clazz, int n) {
-		return (W[]) java.lang.reflect.Array.newInstance(clazz, n);
+	public static <T> T[] create(Class<?> clazz, int n) {
+		return (T[]) java.lang.reflect.Array.newInstance(clazz, n);
 	}
 
 }
