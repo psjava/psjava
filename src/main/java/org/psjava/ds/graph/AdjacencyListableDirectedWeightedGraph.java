ecc69ce276fe29c98c190973daffd7a6c88d4c49
kongbomb <kongbomb.coder@gmail.com>
2013-09-27 16:18:53 +0900

    remove unnecessary

--- src/main/java/org/psjava/ds/graph/AdjacencyListableDirectedWeightedGraph.java
+++
@@ -1,9 +0,0 @@
-package org.psjava.ds.graph;
-
-import org.psjava.ds.Collection;
-
-public interface AdjacencyListableDirectedWeightedGraph<V, W> {
-	Collection<V> getVertices();
-
-	Iterable<DirectedWeightedEdge<V, W>> getEdges(Object from);
-}
