6819854679a5f53306e835441e79987fa57ee0b4
kongbomb <kongbomb.coder@gmail.com>
2013-10-01 16:26:14 +0900

    add dfs example

--- src/main/java/org/psjava/algo/graph/dfs/AllSourceDFS.java
+++ src/main/java/org/psjava/algo/graph/dfs/AllSourceDFS.java
@@ -5,6 +5,9 @@ import org.psjava.ds.graph.Graph;
 
 public class AllSourceDFS {
 
+	/**
+	 * Consider that visiting order is not fixed.
+	 */
 	public static <V, E extends DirectedEdge<V>> void traverse(Graph<V, E> graph, DFSVisitor<V, E> visitor) {
 		MultiSourceDFS.traverse(graph, graph.getVertices(), visitor);
 	}
