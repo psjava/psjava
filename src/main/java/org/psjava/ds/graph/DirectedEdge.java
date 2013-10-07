551ec40064277e98850055022ccdeacbcd32c087
kongbomb <kongbomb.coder@gmail.com>
2013-10-07 23:47:26 +0900

    add comment

--- src/main/java/org/psjava/ds/graph/DirectedEdge.java
+++ src/main/java/org/psjava/ds/graph/DirectedEdge.java
@@ -1,6 +1,6 @@
 package org.psjava.ds.graph;
 
-public interface DirectedEdge<V> {
+public interface DirectedEdge<V> { // TODO introduce Edge interface, and assert existence in MutableGraph
 	V from();
 
 	V to();
