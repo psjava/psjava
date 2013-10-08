8cc55412a8fd5f1b0dee897ef969d128cc8daaaf
kongbomb <kongbomb.coder@gmail.com>
2013-10-08 11:12:26 +0900

    add sieve example

---
+++ src/test/java/org/psjava/example/PrimeNumberSieveExample.java
@@ -0,0 +1,24 @@
+package org.psjava.example;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.psjava.math.numbertheory.PrimeNumberSieve;
+import org.psjava.math.numbertheory.SieveOfEratosthenes;
+
+public class PrimeNumberSieveExample {
+
+	@Test
+	public void example() {
+
+		// Select sieve algorithm, and just call it.
+
+		PrimeNumberSieve algo = SieveOfEratosthenes.getInstance();
+		boolean[] prime = algo.calcPrimeMap(30);
+
+		// 29 is a prime number, 25 is not.
+
+		Assert.assertEquals(true, prime[29]);
+		Assert.assertEquals(false, prime[25]);
+	}
+}
