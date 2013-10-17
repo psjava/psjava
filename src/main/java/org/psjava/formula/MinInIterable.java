18b67e3768914a95e64ff7364c4b066e44f6d290
kongbomb <kongbomb.coder@gmail.com>
2013-10-17 10:02:24 +0900

    add a util

---
+++ src/main/java/org/psjava/formula/MinInIterable.java
@@ -0,0 +1,13 @@
+package org.psjava.formula;
+
+import java.util.Comparator;
+
+import org.psjava.util.ReversedComparator;
+
+public class MinInIterable {
+
+	public static <T> T min(Iterable<T> iterable, Comparator<T> comp) {
+		return MaxInIterable.max(iterable, ReversedComparator.wrap(comp));
+	}
+
+}
