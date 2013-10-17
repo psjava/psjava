5a7b04cb2ef0e12c3db23720eea4f88a3d6446d8
kongbomb <kongbomb.coder@gmail.com>
2013-10-17 17:23:15 +0900

    add a util class

---
+++ src/main/java/org/psjava/algo/math/FunctionFromMap.java
@@ -0,0 +1,17 @@
+package org.psjava.algo.math;
+
+import org.psjava.ds.map.Map;
+import org.psjava.ds.math.Function;
+
+public class FunctionFromMap {
+
+	public static <I, O> Function<I, O> wrap(final Map<I, O> flow) {
+		return new Function<I, O>() {
+			@Override
+			public O get(I input) {
+				return flow.get(input);
+			}
+		};
+	}
+
+}
