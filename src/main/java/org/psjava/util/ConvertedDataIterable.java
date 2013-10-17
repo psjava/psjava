c48f9f1350004e2760129bae4205427b5738c6a4
kongbomb <kongbomb.coder@gmail.com>
2013-10-17 11:20:38 +0900

    add annotation

--- src/main/java/org/psjava/util/ConvertedDataIterable.java
+++ src/main/java/org/psjava/util/ConvertedDataIterable.java
@@ -8,6 +8,7 @@ public class ConvertedDataIterable {
 	
 	public static <T1, T2> Iterable<T2> create(final Iterable<T1> outerIterable, final DataConverter<T1, T2> converter) {
 		return new Iterable<T2>() {
+			@Override
 			public Iterator<T2> iterator() {
 				return ConvertedDataIterator.create(outerIterable.iterator(), converter);			
 			}
