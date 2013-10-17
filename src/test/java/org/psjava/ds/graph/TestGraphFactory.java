0b5abd903e1666b71a5f4741af390559f857b5a9
kongbomb <kongbomb.coder@gmail.com>
2013-10-17 15:12:36 +0900

    add a test util method

--- src/test/java/org/psjava/ds/graph/TestGraphFactory.java
+++ src/test/java/org/psjava/ds/graph/TestGraphFactory.java
@@ -3,7 +3,7 @@ package org.psjava.ds.graph;
 public class TestGraphFactory {
 
 	// TODO rename createDirectedWeighted
-	public static MutableDirectedWeightedGraph<Integer, Integer> create(int[][] edata) { 
+	public static MutableDirectedWeightedGraph<Integer, Integer> create(int[][] edata) {
 		MutableDirectedWeightedGraph<Integer, Integer> g = new MutableDirectedWeightedGraph<Integer, Integer>();
 		for (int[] e : edata) {
 			g.insertVertex(e[0]);
@@ -19,6 +19,16 @@ public class TestGraphFactory {
 			g.insertVertex(e[0]);
 			g.insertVertex(e[1]);
 			g.addEdge(e[0], e[1]);
+		}
+		return g;
+	}
+
+	public static Graph<String, CapacityEdge<String, Integer>> createCapacityGraph(Object[][] edata) {
+		MutableCapacityGraph<String, Integer> g = MutableCapacityGraph.create();
+		for (Object[] e : edata) {
+			g.insertVertex((String) e[0]);
+			g.insertVertex((String) e[1]);
+			g.addEdge((String) e[0], (String) e[1], (Integer) e[2]);
 		}
 		return g;
 	}
