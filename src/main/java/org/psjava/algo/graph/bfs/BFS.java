eb3ef847d6374e704ffbdf55f8f516dc2778701c
kongbomb <kongbomb.coder@gmail.com>
2013-10-18 10:12:32 +0900

    separate class to use also in dfs

--- src/main/java/org/psjava/algo/graph/bfs/BFS.java
+++ src/main/java/org/psjava/algo/graph/bfs/BFS.java
@@ -8,7 +8,6 @@ import org.psjava.ds.set.MutableSet;
 import org.psjava.ds.set.MutableSetFactory;
 import org.psjava.goods.GoodMutableSetFactory;
 import org.psjava.goods.GoodQueueFactory;
-import org.psjava.util.VisitorStopper;
 
 /**
  * Implementation of Breadth-first search
@@ -20,15 +19,6 @@ public class BFS {
 
 	private static final QueueFactory QF = GoodQueueFactory.getInstance();
 	private static final MutableSetFactory SF = GoodMutableSetFactory.getInstance();
-
-	private static class Stopper implements VisitorStopper {
-		boolean stopped;
-
-		@Override
-		public void stop() {
-			stopped = true;
-		}
-	}
 
 	private static class QueueItem<V> {
 		int depth;
@@ -42,17 +32,17 @@ public class BFS {
 
 	public static <V, E extends DirectedEdge<V>> void traverse(AdjacencyList<V, E> adj, Iterable<V> starts, BFSVisitor<V, E> visitor) {
 		MutableSet<V> discovered = SF.create();
-		Stopper stopper = new Stopper();
+		SimpleStopper stopper = new SimpleStopper();
 		Queue<QueueItem<V>> queue = QF.create();
 		for (V v : starts) {
 			queue.enque(new QueueItem<V>(0, v));
 			discovered.insert(v);
 			visitor.onDiscover(v, 0, stopper);
-			if (stopper.stopped)
+			if (stopper.isStopped())
 				break;
 		}
 
-		while (!queue.isEmpty() && !stopper.stopped) {
+		while (!queue.isEmpty() && !stopper.isStopped()) {
 			QueueItem<V> cur = queue.deque();
 			for (E edge : adj.getEdges(cur.v)) {
 				V nextv = edge.to();
@@ -60,7 +50,7 @@ public class BFS {
 					discovered.insert(nextv);
 					visitor.onWalk(edge);
 					visitor.onDiscover(nextv, cur.depth + 1, stopper);
-					if (stopper.stopped)
+					if (stopper.isStopped())
 						break;
 					queue.enque(new QueueItem<V>(cur.depth + 1, nextv));
 				}
---
+++ src/main/java/org/psjava/algo/graph/bfs/SimpleStopper.java
@@ -0,0 +1,16 @@
+package org.psjava.algo.graph.bfs;
+
+import org.psjava.util.VisitorStopper;
+
+public class SimpleStopper implements VisitorStopper {
+	private boolean stopped;
+
+	@Override
+	public void stop() {
+		stopped = true;
+	}
+
+	public boolean isStopped() {
+		return stopped;
+	}
+}
