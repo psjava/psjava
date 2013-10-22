3b32c936f375c6f11e713ae56f58472d2925e401
kongbomb <kongbomb.coder@gmail.com>
2013-10-22 10:55:23 +0900

    add a example

---
+++ src/test/java/org/psjava/example/QueueExample.java
@@ -0,0 +1,39 @@
+package org.psjava.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.psjava.ds.deque.DoubleLinkedListFactory;
+import org.psjava.ds.queue.Queue;
+import org.psjava.ds.queue.QueueFactory;
+import org.psjava.ds.queue.QueueFactoryUsingDeque;
+import org.psjava.goods.GoodQueueFactory;
+
+public class QueueExample {
+
+	@Test
+	public void example() {
+
+		QueueFactory queueFactory = QueueFactoryUsingDeque.create(new DoubleLinkedListFactory());
+
+		Queue<String> queue = queueFactory.create();
+
+		queue.enque("A");
+		queue.enque("B");
+
+		// Here are operations.
+
+		String deque1 = queue.deque();
+		String deque2 = queue.deque();
+
+		boolean empty = queue.isEmpty();
+
+		// There are several implementations.
+
+		QueueFactoryUsingDeque.create(new DoubleLinkedListFactory());
+		GoodQueueFactory.getInstance(); // We choose a good one.
+
+		Assert.assertEquals("A", deque1);
+		Assert.assertEquals("B", deque2);
+		Assert.assertTrue(empty);
+	}
+}
