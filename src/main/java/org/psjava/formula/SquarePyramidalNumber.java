4b132fca5deaf77f259f192659f98861f5e8940e
kongbomb <kongbomb.coder@gmail.com>
2013-11-28 14:01:56 +0900

    add square pyramical number calc

---
+++ src/main/java/org/psjava/formula/SquarePyramidalNumber.java
@@ -0,0 +1,20 @@
+package org.psjava.formula;
+
+import org.psjava.ds.numbersystrem.IntegerDivisableNumberSystem;
+
+/**
+ * http://en.wikipedia.org/wiki/Square_pyramidal_number
+ */
+
+public class SquarePyramidalNumber {
+	@SuppressWarnings("unchecked")
+	public static <T> T calc(T n, IntegerDivisableNumberSystem<T> ns) {
+		T t1 = n;
+		T t2 = ns.add(n, ns.getOne());
+		T t3 = SumOfVarargs.calc(ns, n, n, ns.getOne());
+		return ns.integerDivide(ProductOfVarargs.product(ns, t1, t2, t3), ns.getByInt(6));
+	}
+
+	private SquarePyramidalNumber() {
+	}
+}
---
+++ src/test/java/org/psjava/formula/SquarePyramidalNumberTest.java
@@ -0,0 +1,12 @@
+package org.psjava.formula;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.psjava.ds.numbersystrem.IntegerNumberSystem;
+
+public class SquarePyramidalNumberTest {
+	@Test
+	public void test() {
+		Assert.assertEquals(55, (int) SquarePyramidalNumber.calc(5, IntegerNumberSystem.getInstance()));
+	}
+}
