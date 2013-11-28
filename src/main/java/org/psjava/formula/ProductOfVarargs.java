7f8ba79d9bdcc454667dcb5a988456d41c45825d
kongbomb <kongbomb.coder@gmail.com>
2013-11-28 14:01:43 +0900

    add a calc. product of varargs.

---
+++ src/main/java/org/psjava/formula/ProductOfVarargs.java
@@ -0,0 +1,15 @@
+package org.psjava.formula;
+
+import org.psjava.ds.numbersystrem.MultipliableNumberSystem;
+
+public class ProductOfVarargs {
+	public static <T> T product(MultipliableNumberSystem<T> ns, T... v) {
+		T r = ns.getOne();
+		for (T a : v)
+			r = ns.multiply(r, a);
+		return r;
+	}
+
+	private ProductOfVarargs() {
+	}
+}
