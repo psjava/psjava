4c01a07888999d32c1c31f255a07bfb62ea3c395
kongbomb <kongbomb.coder@gmail.com>
2013-12-04 18:15:23 +0900

    add a small calc: average

---
+++ src/main/java/org/psjava/formula/Average.java
@@ -0,0 +1,15 @@
+package org.psjava.formula;
+
+import org.psjava.ds.Collection;
+import org.psjava.ds.numbersystrem.DivisableNumberSystem;
+
+public class Average {
+
+	public static <T> T calc(Collection<T> a, DivisableNumberSystem<T> ns) {
+		T sum = Sum.calc(ns, a);
+		return ns.divide(sum, ns.getByInt(a.size()));
+	}
+
+	private Average() {
+	}
+}
