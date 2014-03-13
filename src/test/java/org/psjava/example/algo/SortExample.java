8dace058157e5a79b05f7572b9722e2131b7395a
kongbomb <kongbomb.coder@gmail.com>
2014-03-13 13:57:45 +0900

    make sort example more simpler

--- src/test/java/org/psjava/example/algo/SortExample.java
+++ src/test/java/org/psjava/example/algo/SortExample.java
@@ -3,14 +3,10 @@ package org.psjava.example.algo;
 import junit.framework.Assert;
 
 import org.junit.Test;
-import org.psjava.algo.sequence.sort.BubbleSort;
-import org.psjava.algo.sequence.sort.InsertionSort;
 import org.psjava.algo.sequence.sort.MergeSort;
-import org.psjava.algo.sequence.sort.RandomizedQuicksort;
 import org.psjava.algo.sequence.sort.SortingHelper;
 import org.psjava.ds.array.MutableArray;
 import org.psjava.ds.array.MutableArrayFromValues;
-import org.psjava.goods.GoodSortingAlgorithm;
 import org.psjava.util.DefaultComparator;
 
 /**
@@ -23,26 +19,18 @@ public class SortExample {
 	@Test
 	public void example() {
 
-		// Following example is normal usage.
+		// Following example is a normal usage.
 
 		MutableArray<Integer> array1 = MutableArrayFromValues.create(2, 1, 3);
 		MergeSort.getInstance().sort(array1, new DefaultComparator<Integer>());
+		Assert.assertEquals("(1,2,3)", array1.toString());
 
-		// There is only one method in Sort interface,
-		// but there are several convenient methods in SortHelper class
-		// Following is partial sorting.
+		// There is only one method in the 'Sort' interface,
+		// but there are several convenient methods in 'SortHelper' class
+		// Following is a partial sorting example.
 
 		MutableArray<Integer> array2 = MutableArrayFromValues.create(100, 3, 2, 1, 0);
 		SortingHelper.sort(MergeSort.getInstance(), array2, 1, 4);
-
-		// There are several implementations.
-		RandomizedQuicksort.getInstance();
-		InsertionSort.getInstance();
-		BubbleSort.getInstance();
-		MergeSort.getInstance();
-		GoodSortingAlgorithm.getInstance(); // We choose a good one for you.
-
-		Assert.assertEquals("(1,2,3)", array1.toString());
 		Assert.assertEquals("(100,1,2,3,0)", array2.toString());
 	}
 }
