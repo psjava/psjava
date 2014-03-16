a90a120fbf48fe22e93cb4c5f259de5d2f5dd655
kongbomb <kongbomb.coder@gmail.com>
2014-03-17 00:42:13 +0900

    fix link in example doc

--- src/test/java/org/psjava/example/algo/MergeSortExample.java
+++ src/test/java/org/psjava/example/algo/MergeSortExample.java
@@ -4,13 +4,12 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 import org.psjava.algo.sequence.sort.MergeSort;
-import org.psjava.algo.sequence.sort.RandomizedQuicksort;
 import org.psjava.ds.array.MutableArray;
 import org.psjava.ds.array.MutableArrayFromValues;
 import org.psjava.util.DefaultComparator;
 
 /**
- * @implementation {@link RandomizedQuicksort}
+ * @implementation {@link MergeSort}
  * @see {@link SortingAlgorithmExample}
  */
 public class MergeSortExample {
