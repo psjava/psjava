b35d55eb22418f08a8243b80b6b89b8a493f3ad3
kongbomb <kongbomb.coder@gmail.com>
2014-03-18 16:11:44 +0900

    add abs unit test

--- src/main/java/org/psjava/formula/Abs.java
+++ src/main/java/org/psjava/formula/Abs.java
@@ -4,10 +4,13 @@ import org.psjava.ds.numbersystrem.AddInvert;
 import org.psjava.ds.numbersystrem.AddableNumberSystem;
 
 public class Abs {
-	// TODO add unit test
+
 	public static <T> T calc(AddableNumberSystem<T> ns, T v) {
 		if (ns.isNegative(v))
 			return AddInvert.calc(ns, v);
 		return v;
+	}
+
+	private Abs() {
 	}
 }
---
+++ src/test/java/org/psjava/formula/AbsTest.java
@@ -0,0 +1,14 @@
+package org.psjava.formula;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.psjava.ds.numbersystrem.IntegerNumberSystem;
+
+public class AbsTest {
+	@Test
+	public void test() {
+		Assert.assertEquals(100, (int) Abs.calc(IntegerNumberSystem.getInstance(), 100));
+		Assert.assertEquals(100, (int) Abs.calc(IntegerNumberSystem.getInstance(), -100));
+		Assert.assertEquals(0, (int) Abs.calc(IntegerNumberSystem.getInstance(), 0));
+	}
+}
