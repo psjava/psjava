1cdb2461c91d3dcf0f3921715daad85b2168def3
kongbomb <kongbomb.coder@gmail.com>
2014-03-20 00:03:37 +0900

    add simple utils

---
+++ src/main/java/org/psjava/util/Quadruple.java
@@ -0,0 +1,42 @@
+package org.psjava.util;
+
+import org.psjava.algo.math.PairHash;
+
+public class Quadruple<T1, T2, T3, T4> implements EqualityTester<Quadruple<T1, T2, T3, T4>> {
+
+	public static <T1, T2, T3, T4> Quadruple<T1, T2, T3, T4> create(T1 v1, T2 v2, T3 v3, T4 v4) {
+		return new Quadruple<T1, T2, T3, T4>(v1, v2, v3, v4);
+	}
+
+	public final T1 v1;
+	public final T2 v2;
+	public final T3 v3;
+	public final T4 v4;
+
+	public Quadruple(T1 v1, T2 v2, T3 v3, T4 v4) {
+		this.v1 = v1;
+		this.v2 = v2;
+		this.v3 = v3;
+		this.v4 = v4;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return StrictEqualityTester.areEqual(this, obj, this);
+	}
+
+	@Override
+	public boolean areEqual(Quadruple<T1, T2, T3, T4> o1, Quadruple<T1, T2, T3, T4> o2) {
+		return o1.v1.equals(o2.v1) && o1.v2.equals(o2.v2) && o1.v3.equals(o2.v3) && o1.v4.equals(o2.v4);
+	}
+
+	@Override
+	public int hashCode() {
+		return PairHash.hash(PairHash.hash(v1.hashCode(), v2.hashCode()), PairHash.hash(v3.hashCode(), v4.hashCode()));
+	}
+
+	@Override
+	public String toString() {
+		return "(" + v1 + "," + v2 + "," + v3 + "," + v4 + ")";
+	}
+}
---
+++ src/main/java/org/psjava/util/StringReverser.java
@@ -0,0 +1,16 @@
+package org.psjava.util;
+
+import org.psjava.ds.array.ArrayReverser;
+import org.psjava.ds.array.MutableArrayUsingCharArray;
+
+public class StringReverser {
+
+	public static String getReversed(String a) {
+		char[] temp = a.toCharArray();
+		ArrayReverser.reverse(MutableArrayUsingCharArray.wrap(temp));
+		return new String(temp);
+	}
+
+	private StringReverser() {
+	}
+}
