d0c37831eb13828c43cda1df11463eaf943331aa
kongbomb <kongbomb.coder@gmail.com>
2014-04-20 23:36:36 +0900

    add Shuffler

---
+++ src/main/java/org/psjava/algo/sequence/Shuffler.java
@@ -0,0 +1,20 @@
+package org.psjava.algo.sequence;
+
+import java.util.Random;
+
+import org.psjava.ds.array.ArraySwapper;
+import org.psjava.ds.array.MutableArray;
+import org.psjava.util.ZeroTo;
+
+public class Shuffler {
+
+	private static final Random RANDOM = new Random();
+
+	public static <T> void shuffle(MutableArray<T> a) {
+		for (int i : ZeroTo.get(a.size()))
+			ArraySwapper.swap(a, i, i + RANDOM.nextInt(a.size() - i));
+	}
+
+	private Shuffler() {
+	}
+}
