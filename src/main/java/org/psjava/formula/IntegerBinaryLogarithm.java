b55fbeda94f04f5c987c45f1a1d65cfa2ede2311
kongbomb <kongbomb.coder@gmail.com>
2014-04-24 11:05:03 +0900

    improve debug log

--- src/main/java/org/psjava/formula/IntegerBinaryLogarithm.java
+++ src/main/java/org/psjava/formula/IntegerBinaryLogarithm.java
@@ -5,7 +5,7 @@ import org.psjava.util.AssertStatus;
 public class IntegerBinaryLogarithm {
 
 	public static int calc(int v) {
-		AssertStatus.assertTrue(v >= 1);
+		AssertStatus.assertTrue(v >= 1, "Logarithm argument must be positive");
 		return 31 - Integer.numberOfLeadingZeros(v);
 	}
 
