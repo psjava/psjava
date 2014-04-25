d22c7b955f9f48cda21d4085bf2b52fe4d4aee25
kongbomb <kongbomb.coder@gmail.com>
2014-04-26 00:45:49 +0900

    add undirected graph support in AdjacencyListFromGraph

--- src/main/java/org/psjava/ds/graph/AdjacencyList.java
+++ src/main/java/org/psjava/ds/graph/AdjacencyList.java
@@ -2,7 +2,7 @@ package org.psjava.ds.graph;
 
 import org.psjava.ds.Collection;
 
-public interface AdjacencyList<V, E extends DirectedEdge<V>> {
+public interface AdjacencyList<V, E> {
 	Collection<V> getVertices();
 
 	Iterable<E> getEdges(V from);
--- src/main/java/org/psjava/ds/graph/AdjacencyListFromGraph.java
+++ src/main/java/org/psjava/ds/graph/AdjacencyListFromGraph.java
@@ -2,6 +2,7 @@ package org.psjava.ds.graph;
 
 import org.psjava.ds.Collection;
 import org.psjava.ds.array.DynamicArray;
+import org.psjava.ds.map.Map;
 import org.psjava.ds.map.MutableMap;
 import org.psjava.goods.GoodMutableMapFactory;
 import org.psjava.util.IterableToString;
@@ -9,15 +10,33 @@ import org.psjava.util.IterableToString;
 public class AdjacencyListFromGraph {
 
 	public static <V, E extends DirectedEdge<V>> AdjacencyList<V, E> create(final Graph<V, E> g) {
-		final MutableMap<V, DynamicArray<E>> index = GoodMutableMapFactory.getInstance().create();
-		for (V v : g.getVertices())
-			index.put(v, new DynamicArray<E>());
+		Map<V, DynamicArray<E>> index = initIndexMap(g.getVertices());
 		for (E e : g.getEdges())
 			index.get(e.from()).addToLast(e);
+		return create(g.getVertices(), index);
+	}
+
+	public static <V, E extends UndirectedEdge<V>> AdjacencyList<V, E> createFromUndirected(final Graph<V, E> g) {
+		Map<V, DynamicArray<E>> index = initIndexMap(g.getVertices());
+		for (E e : g.getEdges()) {
+			index.get(e.v1()).addToLast(e);
+			index.get(e.v2()).addToLast(e);
+		}
+		return create(g.getVertices(), index);
+	}
+
+	private static <V, E> Map<V, DynamicArray<E>> initIndexMap(final Collection<V> vertices) {
+		MutableMap<V, DynamicArray<E>> index = GoodMutableMapFactory.getInstance().create();
+		for (V v : vertices)
+			index.put(v, new DynamicArray<E>());
+		return index;
+	}
+
+	private static <V, E> AdjacencyList<V, E> create(final Collection<V> vertices, final Map<V, DynamicArray<E>> index) {
 		return new AdjacencyList<V, E>() {
 			@Override
 			public Collection<V> getVertices() {
-				return g.getVertices();
+				return vertices;
 			}
 
 			@Override
@@ -27,12 +46,15 @@ public class AdjacencyListFromGraph {
 
 			@Override
 			public String toString() {
-				String r = "Graph({" + IterableToString.toString(g.getVertices()) + "},{";
+				String r = "Graph({" + IterableToString.toString(vertices) + "},{";
 				for (V v : getVertices())
 					r += IterableToString.toString(getEdges(v));
 				return r + "})";
 			}
 		};
+	}
+
+	private AdjacencyListFromGraph() {
 	}
 
 }
