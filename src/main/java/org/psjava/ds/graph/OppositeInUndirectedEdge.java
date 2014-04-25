1b4b910d08fd33d50ff588c09cb5a12486d7e977
kongbomb <kongbomb.coder@gmail.com>
2014-04-26 01:20:04 +0900

    add a graph util - OppositeInUndirectedEdge

---
+++ src/main/java/org/psjava/ds/graph/OppositeInUndirectedEdge.java
@@ -0,0 +1,13 @@
+package org.psjava.ds.graph;
+
+
+public class OppositeInUndirectedEdge {
+
+	public static <V, E extends UndirectedEdge<V>> V get(E e, V v) {
+		return v.equals(e.v1()) ? e.v2() : e.v1();
+	}
+
+	private OppositeInUndirectedEdge() {
+	}
+
+}
