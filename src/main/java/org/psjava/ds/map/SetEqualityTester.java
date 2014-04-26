9bcbe9aced2035e3e0d33469250b341bd06f8a06
kongbomb <kongbomb.coder@gmail.com>
2014-04-26 23:39:03 +0900

    add SetEqualityTester

---
+++ src/main/java/org/psjava/ds/map/SetEqualityTester.java
@@ -0,0 +1,19 @@
+package org.psjava.ds.map;
+
+import org.psjava.ds.set.Set;
+
+public class SetEqualityTester {
+
+	public static <V> boolean areEqual(Set<V> m1, Set<V> m2) {
+		if (m1.size() != m2.size())
+			return false;
+		for (V v : m1)
+			if (!m2.contains(v))
+				return false;
+		return true;
+	}
+
+	private SetEqualityTester() {
+	}
+
+}
