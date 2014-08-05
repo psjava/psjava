6a0cc6cfdf5f117c204963baaff7c72ccc70baf4
kongbomb <kongbomb.coder@gmail.com>
2014-08-05 23:11:34 +0900

    add new graph's method for EdgeFilteredSubGraph

--- src/main/java/org/psjava/ds/graph/EdgeFilteredSubGraph.java
+++ src/main/java/org/psjava/ds/graph/EdgeFilteredSubGraph.java
@@ -6,6 +6,7 @@ import org.psjava.util.FilteredIterable;
 
 public class EdgeFilteredSubGraph {
 
+	@Deprecated
 	public static <V, E> OldGraph<V, E> wrap(final OldGraph<V, E> original, final DataFilter<E> filter) {
 		return new OldGraph<V, E>() {
 			@Override
@@ -16,6 +17,25 @@ public class EdgeFilteredSubGraph {
 			@Override
 			public Iterable<E> getEdges() {
 				return FilteredIterable.create(original.getEdges(), filter);
+			}
+
+			@Override
+			public String toString() {
+				return GraphToString.toString(this);
+			}
+		};
+	}
+
+	public static <V, E> Graph<V, E> wrap(final Graph<V, E> original, final DataFilter<E> filter) {
+		return new Graph<V, E>() {
+			@Override
+			public Collection<V> getVertices() {
+				return original.getVertices();
+			}
+
+			@Override
+			public Iterable<E> getEdges(V v) {
+				return FilteredIterable.create(original.getEdges(v), filter);
 			}
 
 			@Override
