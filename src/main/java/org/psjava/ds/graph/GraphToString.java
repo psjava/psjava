92845a799c139d7490cd7dafa77c6e065a5bde35
kongbomb <kongbomb.coder@gmail.com>
2014-08-06 00:52:58 +0900

    remove not using codes

--- src/main/java/org/psjava/ds/graph/GraphToString.java
+++ src/main/java/org/psjava/ds/graph/GraphToString.java
@@ -4,17 +4,6 @@ import org.psjava.util.IterableToString;
 
 public class GraphToString {
 
-	@Deprecated
-	public static <V, E> String toString(OldGraph<V, E> g) {
-		String r = "";
-		for (E e : g.getEdges()) {
-			if (r.length() != 0)
-				r += ", ";
-			r += e.toString();
-		}
-		return "Graph({" + IterableToString.toString(g.getVertices()) + "},{" + IterableToString.toString(g.getEdges()) + "})";
-	}
-
 	public static <V, E> String toString(Graph<V, E> g) {
 		return "Graph({" + IterableToString.toString(g.getVertices()) + "},{" + IterableToString.toString(AllEdgeInGraph.wrap(g)) + "})";
 	}
--- src/test/java/org/psjava/ds/graph/TestGraphFactory.java
+++ src/test/java/org/psjava/ds/graph/TestGraphFactory.java
@@ -2,17 +2,6 @@ package org.psjava.ds.graph;
 
 public class TestGraphFactory {
 
-	@Deprecated // Use String key graph.
-	public static MutableDirectedWeightedOldGraph<Integer, Integer> createDirectedWeighted(int[][] edata) {
-		MutableDirectedWeightedOldGraph<Integer, Integer> g = new MutableDirectedWeightedOldGraph<Integer, Integer>();
-		for (int[] e : edata) {
-			g.insertVertex(e[0]);
-			g.insertVertex(e[1]);
-			g.addEdge(e[0], e[1], e[2]);
-		}
-		return g;
-	}
-
 	public static MutableDirectedWeightedGraph<Integer, Integer> createDirectedWeightedNew(int[][] edata) {
 		MutableDirectedWeightedGraph<Integer, Integer> g = new MutableDirectedWeightedGraph<Integer, Integer>();
 		for (int[] e : edata) {
@@ -23,30 +12,8 @@ public class TestGraphFactory {
 		return g;
 	}
 
-	@Deprecated
-	public static OldGraph<String, DirectedEdge<String>> createDirected(String[][] edata) {
-		MutableDirectedOldGraph<String> g = MutableDirectedOldGraph.create();
-		for (String[] e : edata) {
-			g.insertVertex(e[0]);
-			g.insertVertex(e[1]);
-			g.addEdge(e[0], e[1]);
-		}
-		return g;
-	}
-
 	public static Graph<String, DirectedEdge<String>> createDirectedNew(String[][] edata) {
 		MutableDirectedUnweightedGraph<String> g = MutableDirectedUnweightedGraph.create();
-		for (String[] e : edata) {
-			g.insertVertex(e[0]);
-			g.insertVertex(e[1]);
-			g.addEdge(e[0], e[1]);
-		}
-		return g;
-	}
-
-	@Deprecated
-	public static OldGraph<String, UndirectedEdge<String>> createUndirected(String[][] edata) {
-		MutableUndirectedOldGraph<String> g = MutableUndirectedOldGraph.create();
 		for (String[] e : edata) {
 			g.insertVertex(e[0]);
 			g.insertVertex(e[1]);
@@ -65,17 +32,6 @@ public class TestGraphFactory {
 		return g;
 	}
 
-	@Deprecated
-	public static OldGraph<String, CapacityEdge<String, Integer>> createCapacityGraph(Object[][] edata) {
-		MutableCapacityOldGraph<String, Integer> g = MutableCapacityOldGraph.create();
-		for (Object[] e : edata) {
-			g.insertVertex((String) e[0]);
-			g.insertVertex((String) e[1]);
-			g.addEdge((String) e[0], (String) e[1], (Integer) e[2]);
-		}
-		return g;
-	}
-
 	public static Graph<String, CapacityEdge<String, Integer>> createCapacityGraphNew(Object[][] edata) {
 		MutableCapacityGraph<String, Integer> g = MutableCapacityGraph.create();
 		for (Object[] e : edata) {
@@ -86,34 +42,12 @@ public class TestGraphFactory {
 		return g;
 	}
 
-	@Deprecated
-	public static MutableDirectedWeightedOldGraph<String, Integer> createDirectedWeighted(Object[][] data) {
-		MutableDirectedWeightedOldGraph<String, Integer> g = MutableDirectedWeightedOldGraph.create();
-		for (Object[] e1 : data) {
-			g.insertVertex((String) e1[0]);
-			g.insertVertex((String) e1[1]);
-			g.addEdge((String) e1[0], (String) e1[1], (int) (Integer) e1[2]);
-		}
-		return g;
-	}
-
 	public static MutableDirectedWeightedGraph<String, Integer> createDirectedWeightedNew(Object[][] data) {
 		MutableDirectedWeightedGraph<String, Integer> g = MutableDirectedWeightedGraph.create();
 		for (Object[] e1 : data) {
 			g.insertVertex((String) e1[0]);
 			g.insertVertex((String) e1[1]);
 			g.addEdge((String) e1[0], (String) e1[1], (int) (Integer) e1[2]);
-		}
-		return g;
-	}
-
-	@Deprecated
-	public static MutableUndirectedWeightedOldGraph<String, Integer> createUndirectedWeighted(Object[][] data) {
-		MutableUndirectedWeightedOldGraph<String, Integer> g = MutableUndirectedWeightedOldGraph.create();
-		for (Object[] e1 : data) {
-			g.insertVertex((String) e1[0]);
-			g.insertVertex((String) e1[1]);
-			g.addEdge((String) e1[0], (String) e1[1], (Integer) e1[2]);
 		}
 		return g;
 	}
