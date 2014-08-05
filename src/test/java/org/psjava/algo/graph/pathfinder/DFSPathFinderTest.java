6da2152678081e85a346dde4b505a34d7c9f5414
kongbomb <kongbomb.coder@gmail.com>
2014-08-05 22:59:37 +0900

    introduce new graph to DFSPathFinderTest.java

--- src/test/java/org/psjava/algo/graph/pathfinder/DFSPathFinderTest.java
+++ src/test/java/org/psjava/algo/graph/pathfinder/DFSPathFinderTest.java
@@ -3,24 +3,23 @@ package org.psjava.algo.graph.pathfinder;
 import org.junit.Assert;
 import org.junit.Test;
 import org.psjava.ds.Collection;
-import org.psjava.ds.graph.NewGraphFromGraph;
+import org.psjava.ds.graph.Graph;
 import org.psjava.ds.graph.DirectedEdge;
-import org.psjava.ds.graph.OldGraph;
 import org.psjava.ds.graph.TestGraphFactory;
 
 public class DFSPathFinderTest {
 
 	@Test
 	public void test() {
-		OldGraph<String, DirectedEdge<String>> g = TestGraphFactory.createDirected(new String[][] { { "A", "B" }, { "B", "C" }, { "C", "D" } });
-		Collection<DirectedEdge<String>> r = DFSPathFinder.getInstance().find(NewGraphFromGraph.createFromDirected(g), "A", "C", null);
+		Graph<String, DirectedEdge<String>> g = TestGraphFactory.createDirectedNew(new String[][] { { "A", "B" }, { "B", "C" }, { "C", "D" } });
+		Collection<DirectedEdge<String>> r = DFSPathFinder.getInstance().find(g, "A", "C", null);
 		Assert.assertEquals("(A->B,B->C)", r.toString());
 	}
 
 	@Test
 	public void testNoPath() {
-		OldGraph<String, DirectedEdge<String>> g = TestGraphFactory.createDirected(new String[][] { { "A", "B" }, { "C", "D" } });
-		Collection<DirectedEdge<String>> r = DFSPathFinder.getInstance().find(NewGraphFromGraph.createFromDirected(g), "A", "C", null);
+		Graph<String, DirectedEdge<String>> g = TestGraphFactory.createDirectedNew(new String[][] { { "A", "B" }, { "C", "D" } });
+		Collection<DirectedEdge<String>> r = DFSPathFinder.getInstance().find(g, "A", "C", null);
 		Assert.assertNull(r);
 	}
 
