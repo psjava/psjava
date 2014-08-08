1ddce30494f476ab3f2bf5563b537962d93dd9d2
kongbomb <kongbomb.coder@gmail.com>
2014-08-08 10:26:34 +0900

    add KonigTheoremExample.java

---
+++ src/test/java/org/psjava/example/algo/MinimumVertexCoverInBipartiteGraphExample.java
@@ -0,0 +1,17 @@
+package org.psjava.example.algo;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ * @see {@link org.psjava.algo.graph.KonigTheorem}
+ */
+public class MinimumVertexCoverInBipartiteGraphExample {
+
+	@Test
+	public void example() {
+		// The only one implentation of minimum vertex cover algorithm is Konig's Theorem
+		// See the detail in Konig's Theorem example
+	}
+
+}
