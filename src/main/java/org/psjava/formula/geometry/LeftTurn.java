b68cee961719e9b0f19acd9c1da6cc591b3a9681
kongbomb <kongbomb.coder@gmail.com>
2014-12-01 17:08:15 +0900

    add LeftTurn

---
+++ src/main/java/org/psjava/formula/geometry/LeftTurn.java
@@ -0,0 +1,14 @@
+package org.psjava.formula.geometry;
+
+import org.psjava.ds.geometry.Point2D;
+import org.psjava.ds.numbersystrem.MultipliableNumberSystem;
+
+public class LeftTurn {
+
+	public static <T> boolean is(Point2D<T> p1, Point2D<T> p2, Point2D<T> p3, MultipliableNumberSystem<T> ns) {
+		T ccw = CCW.ccw(ns, p1, p2, p3);
+		return org.psjava.formula.geometry.LeftTurnFromCCW.is(ns, ccw);
+	}
+
+	private LeftTurn() {}
+}
---
+++ src/test/java/org/psjava/formula/geometry/LeftTurnTest.java
@@ -0,0 +1,27 @@
+package org.psjava.formula.geometry;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.psjava.ds.geometry.Point2D;
+import org.psjava.ds.numbersystrem.IntegerNumberSystem;
+
+public class LeftTurnTest {
+
+	private static final IntegerNumberSystem NS = IntegerNumberSystem.getInstance();
+
+	@Test
+	public void test() {
+		Assert.assertTrue(LeftTurn.is(toPoint(0, 0), toPoint(0, 10), toPoint(-10, 10), NS));
+		Assert.assertFalse(LeftTurn.is(toPoint(0, 0), toPoint(0, 10), toPoint(10, 10), NS));
+	}
+
+	@Test
+	public void testStraight() {
+		Assert.assertFalse(LeftTurn.is(toPoint(0, 0), toPoint(0, 10), toPoint(0, 20), NS));
+	}
+
+	private static Point2D<Integer> toPoint(int v, int v1) {
+		return Point2D.create(v,v1);
+	}
+
+}
