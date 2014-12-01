fcb6616cf18d56ba3bbbebba82734c099a94af8e
kongbomb <kongbomb.coder@gmail.com>
2014-12-02 00:22:58 +0900

    add PointByYXComparator

---
+++ src/main/java/org/psjava/ds/geometry/PointByYXComparator.java
@@ -0,0 +1,17 @@
+package org.psjava.ds.geometry;
+
+import org.psjava.util.SeriesComparator;
+
+import java.util.Comparator;
+
+public class PointByYXComparator {
+
+	@SuppressWarnings("unchecked")
+	public static <T> Comparator<Point2D<T>> create(Comparator<T> comp) {
+		return SeriesComparator.create(PointByYComparator.create(comp), PointByXComparator.create(comp));
+	}
+
+	private PointByYXComparator() {
+	}
+
+}
