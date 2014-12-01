173733810cc1a71f524bc7bc79552bef4961d13c
kongbomb <kongbomb.coder@gmail.com>
2014-12-01 17:50:05 +0900

    add PointByDistanceComparator

---
+++ src/main/java/org/psjava/formula/geometry/PointByDistanceComparator.java
@@ -0,0 +1,24 @@
+package org.psjava.formula.geometry;
+
+import org.psjava.ds.geometry.Point2D;
+import org.psjava.ds.numbersystrem.MultipliableNumberSystem;
+
+import java.util.Comparator;
+
+public class PointByDistanceComparator{
+
+	public static <T> Comparator<Point2D<T>> create(final Point2D<T> ref, final MultipliableNumberSystem<T> ns) {
+		return new Comparator<Point2D<T>>() {
+			@Override
+			public int compare(Point2D<T> p1, Point2D<T> p2) {
+				return ns.compare(calcDistanceSquare(p1), calcDistanceSquare(p2));
+			}
+			private T calcDistanceSquare(Point2D<T> p) {
+				return DistanceSquareBetweenPoints.calc(ns, ref, p);
+			}
+		};
+	}
+
+	private PointByDistanceComparator() {}
+
+}
