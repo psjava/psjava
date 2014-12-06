7ef59f82cb7bfb4e096cf67030ae2fc2db032786
kongbomb <kongbomb.coder@gmail.com>
2014-12-07 00:08:46 +0900

    add RightTurn checker

---
+++ src/main/java/org/psjava/formula/geometry/RightTurn.java
@@ -0,0 +1,14 @@
+package org.psjava.formula.geometry;
+
+import org.psjava.ds.geometry.Point2D;
+import org.psjava.ds.numbersystrem.MultipliableNumberSystem;
+
+public class RightTurn {
+
+	public static <T> boolean is(Point2D<T> p1, Point2D<T> p2, Point2D<T> p3, MultipliableNumberSystem<T> ns) {
+		T ccw = CCW.ccw(ns, p1, p2, p3);
+		return RightTurnFromCCW.is(ns, ccw);
+	}
+
+	private RightTurn() {}
+}
