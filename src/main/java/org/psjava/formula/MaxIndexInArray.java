a9beb8cc5758283364ef32a488146e869393571e
kongbomb <kongbomb.coder@gmail.com>
2014-12-07 00:13:31 +0900

    add MaxIndexInArray, MinIndexInArray

---
+++ src/main/java/org/psjava/formula/MaxIndexInArray.java
@@ -0,0 +1,20 @@
+package org.psjava.formula;
+
+import java.util.Comparator;
+
+import org.psjava.ds.array.Array;
+import org.psjava.util.AssertStatus;
+import org.psjava.util.ZeroTo;
+
+public class MaxIndexInArray {
+
+	public static <T> int get(Array<T> a, Comparator<T> comp) {
+		int r = -1;
+		for(int i : ZeroTo.get(a.size()))
+			if(r == -1 || comp.compare(a.get(r), a.get(i)) < 0)
+				r = i;
+		AssertStatus.assertTrue(r != -1, "Empty Iterable");
+		return r;
+	}
+
+}
---
+++ src/test/java/org/psjava/formula/MaxIndexInArrayTest.java
@@ -0,0 +1,29 @@
+package org.psjava.formula;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.psjava.ds.array.Array;
+import org.psjava.ds.array.ArrayFromVarargs;
+import org.psjava.ds.array.DynamicArray;
+import org.psjava.util.DefaultComparator;
+
+import java.util.Comparator;
+
+public class MaxIndexInArrayTest {
+
+	@Test
+	public void testNormal() {
+		Array<Integer> a = ArrayFromVarargs.create(1, 3, 2);
+		assertEquals(1, MaxIndexInArray.get(a, COMP));
+	}
+
+	@Test (expected=RuntimeException.class)
+	public void testEmpty() {
+		DynamicArray<Integer> a = DynamicArray.create();
+		MaxIndexInArray.get(a, COMP);
+	}
+
+	private static final Comparator<Integer> COMP = new DefaultComparator<Integer>();
+
+}
