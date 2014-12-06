36a1dd7c4045f135fd3990b81fa7c4a48187e1d7
kongbomb <kongbomb.coder@gmail.com>
2014-12-07 01:20:18 +0900

    add Point2DMovedByDirection

---
+++ src/main/java/org/psjava/formula/geometry/Point2DMovedByDirection.java
@@ -0,0 +1,17 @@
+package org.psjava.formula.geometry;
+
+import org.psjava.ds.geometry.Point2D;
+import org.psjava.ds.math.Vector2D;
+import org.psjava.ds.numbersystrem.AddableNumberSystem;
+
+public class Point2DMovedByDirection {
+
+	public static <T> Point2D<T> get(Point2D<T> p, Vector2D<T> directionVector, AddableNumberSystem<T> ns) {
+		T nx = ns.add(directionVector.x(), p.x());
+		T ny = ns.add(directionVector.y(), p.y());
+		return Point2D.create(nx, ny);
+	}
+
+	private Point2DMovedByDirection() {}
+
+}
