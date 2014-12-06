7a6c0cba81c702cfcb735c568f272baa2be9fcec
kongbomb <kongbomb.coder@gmail.com>
2014-12-07 00:19:26 +0900

    add StraightOrder

---
+++ src/main/java/org/psjava/formula/geometry/StraightOrder.java
@@ -0,0 +1,11 @@
+package org.psjava.formula.geometry;
+
+import org.psjava.ds.geometry.Point2D;
+import org.psjava.ds.numbersystrem.MultipliableNumberSystem;
+
+public class StraightOrder {
+	public static <T> boolean is(Point2D<T> x1, Point2D<T> x2, Point2D<T> x3, MultipliableNumberSystem<T> ns) {
+		T ccw = CCW.ccw(ns, x1, x2, x3);
+		return org.psjava.formula.geometry.StraightOrderFromCCW.is(ns, x1, x2, x3, ccw);
+	}
+}
