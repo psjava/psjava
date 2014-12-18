9f33254ab4a1d6625073c5667f739a7a7035d16c
kongbomb <kongbomb.coder@gmail.com>
2014-12-18 23:14:35 +0900

    simple refactoring - rename

--- src/main/java/org/psjava/ds/geometry/Polygon2D.java
+++ src/main/java/org/psjava/ds/geometry/Polygon2D.java
@@ -2,28 +2,26 @@ package org.psjava.ds.geometry;
 
 import org.psjava.ds.array.Array;
 import org.psjava.ds.array.MutableArrayFromIterable;
-import org.psjava.util.EqualityTester;
-import org.psjava.util.StrictEqualityTester;
 
 public class Polygon2D<T> {
 
-	public static <T> Polygon2D<T> create(Iterable<Point2D<T>> src) {
-		return new Polygon2D<T>(src);
+	public static <T> Polygon2D<T> create(Iterable<Point2D<T>> ccwOrderPoints) {
+		return new Polygon2D<T>(ccwOrderPoints);
 	}
 
-	private final Array<Point2D<T>> points;
+	private final Array<Point2D<T>> ccwOrderPoints;
 
-	private Polygon2D(Iterable<Point2D<T>> src) {
-		points = MutableArrayFromIterable.create(src);
+	private Polygon2D(Iterable<Point2D<T>> ccwOrderPoints) {
+		this.ccwOrderPoints = MutableArrayFromIterable.create(ccwOrderPoints);
 	}
 
 	public Array<Point2D<T>> getPointsAsArray() {
-		return points;
+		return ccwOrderPoints;
 	}
 
 	@Override
 	public String toString() {
-		return "Polygon2D(" + points + ")";
+		return "Polygon2D(" + ccwOrderPoints + ")";
 	}
 
 }
