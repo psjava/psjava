f8331d45768f4bb10b1d711ea56154c84aa46130
kongbomb <kongbomb.coder@gmail.com>
2014-12-23 18:40:56 +0900

    introduce GoodConvexHullAlgorithm

---
+++ src/main/java/org/psjava/goods/GoodConvexHullAlgorithm.java
@@ -0,0 +1,13 @@
+package org.psjava.goods;
+
+import org.psjava.algo.geometry.convexhull.ConvexHullAlgorithm;
+import org.psjava.algo.geometry.convexhull.GrahamScan;
+
+public class GoodConvexHullAlgorithm {
+
+	public static ConvexHullAlgorithm getInstance() {
+		return GrahamScan.getInstance(GoodSortingAlgorithm.getInstance());
+	}
+
+	private GoodConvexHullAlgorithm() {}
+}
