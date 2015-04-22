20ba2e6f6af0beee0d9c4f66b797b7e9144e2bc3
kongbomb <kongbomb.coder@gmail.com>
2015-04-22 15:03:36 +0900

    add a formula - point on polygon 2d

---
+++ src/main/java/org/psjava/formula/geometry/PointOnPolygon2D.java
@@ -0,0 +1,25 @@
+package org.psjava.formula.geometry;
+
+import org.psjava.ds.array.Array;
+import org.psjava.ds.geometry.Point2D;
+import org.psjava.ds.geometry.Polygon2D;
+import org.psjava.ds.geometry.Segment2D;
+import org.psjava.ds.numbersystrem.MultipliableNumberSystem;
+import org.psjava.util.ZeroTo;
+
+public class PointOnPolygon2D {
+
+	public static <T> boolean isOn(Point2D<T> point, Polygon2D<T> polygon, MultipliableNumberSystem<T> ns) {
+		Array<Point2D<T>> points = polygon.getCCWOrderPoints();
+		if(points.size() == 1)
+			return point.equals(points.get(0));
+		for (int i : ZeroTo.get(points.size())) {
+			Point2D<T> p1 = points.get(i);
+			Point2D<T> p2 = points.get((i + 1) % points.size());
+			if (PointOnSegment2D.isOn(point, Segment2D.create(p1, p2), ns))
+				return true;
+		}
+		return false;
+	}
+
+}
---
+++ src/test/java/org/psjava/formula/geometry/PointOnPolygon2DTest.java
@@ -0,0 +1,23 @@
+package org.psjava.formula.geometry;
+
+import org.junit.Test;
+import org.psjava.ds.geometry.Point2D;
+import org.psjava.ds.geometry.Polygon2D;
+import org.psjava.ds.numbersystrem.IntegerNumberSystem;
+import org.psjava.util.VarargsIterable;
+
+import static org.junit.Assert.*;
+
+public class PointOnPolygon2DTest {
+
+	public static final IntegerNumberSystem NS = IntegerNumberSystem.getInstance();
+
+	@Test
+	public void testOnePointPolygon() {
+		Iterable<Point2D<Integer>> points = VarargsIterable.create(Point2D.create(0, 0));
+		Polygon2D<Integer> poly = Polygon2D.create(points);
+		assertTrue(PointOnPolygon2D.isOn(Point2D.create(0, 0), poly, NS));
+		assertFalse(PointOnPolygon2D.isOn(Point2D.create(1, 1), poly, NS));
+	}
+
+}
