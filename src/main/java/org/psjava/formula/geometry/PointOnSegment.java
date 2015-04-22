67c62f9154e8fd97ad9f34f3637b3c08a0b046b3
kongbomb <kongbomb.coder@gmail.com>
2015-04-22 14:48:49 +0900

    add a formula - PointOnSegment.java

---
+++ src/main/java/org/psjava/formula/geometry/PointOnSegment.java
@@ -0,0 +1,13 @@
+package org.psjava.formula.geometry;
+
+import org.psjava.ds.geometry.Point2D;
+import org.psjava.ds.geometry.Segment2D;
+import org.psjava.ds.numbersystrem.MultipliableNumberSystem;
+
+public class PointOnSegment {
+
+	public static <T> boolean isOn(Point2D<T> p, Segment2D<T> s, MultipliableNumberSystem<T> ns) {
+		return StraightOrder.is(s.p1(), p, s.p2(), ns);
+	}
+
+}
---
+++ src/test/java/org/psjava/formula/geometry/PointOnSegmentTest.java
@@ -0,0 +1,22 @@
+package org.psjava.formula.geometry;
+
+import org.junit.Test;
+import org.psjava.ds.geometry.Point2D;
+import org.psjava.ds.geometry.Segment2D;
+import org.psjava.ds.numbersystrem.IntegerNumberSystem;
+
+import static org.junit.Assert.*;
+
+public class PointOnSegmentTest {
+
+	public static final IntegerNumberSystem NS = IntegerNumberSystem.getInstance();
+
+	@Test
+	public void test() {
+		Segment2D<Integer> s = Segment2D.create(Point2D.create(0, 0), Point2D.create(10, 0));
+		assertTrue(PointOnSegment.isOn(Point2D.create(0, 0), s, NS));
+		assertTrue(PointOnSegment.isOn(Point2D.create(5, 0), s, NS));
+		assertFalse(PointOnSegment.isOn(Point2D.create(11, 0), s, NS));
+	}
+
+}
