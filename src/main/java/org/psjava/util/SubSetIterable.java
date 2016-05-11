e151e42a78a7f2684d4afdd17af92d92d6284076
kongbomb <kongbomb.coder@gmail.com>
2016-05-11 17:46:11 +0900

    add simple comment

--- src/main/java/org/psjava/util/SubSetIterable.java
+++ src/main/java/org/psjava/util/SubSetIterable.java
@@ -11,6 +11,7 @@ public class SubSetIterable {
 		return ConvertedIterable.create(ZeroTo.get(1 << array.size()), new Converter<Integer, Iterable<T>>() {
 			@Override
 			public Iterable<T> convert(final Integer bits) {
+				// Cannot get size of the sub st at this time. Because we use filter way
 				return ConvertedIterable.create(FilteredIterable.create(ZeroTo.get(array.size()), new Filter<Integer>() {
 					@Override
 					public boolean isAccepted(Integer index) {
