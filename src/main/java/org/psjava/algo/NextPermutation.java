58ac9d0f25b3367d7299d1db3eafe90251828167
kongbomb <kongbomb.coder@gmail.com>
2016-05-11 17:53:12 +0900

    add next permutation

---
+++ src/main/java/org/psjava/algo/NextPermutation.java
@@ -0,0 +1,32 @@
+package org.psjava.algo;
+
+import org.psjava.ds.array.ArrayReverser;
+import org.psjava.ds.array.ArraySwapper;
+import org.psjava.ds.array.MutableArray;
+
+import java.util.Comparator;
+
+public class NextPermutation {
+
+	public static <T> boolean step(MutableArray<T> array, Comparator<T> comparator) {
+		int p = -1;
+		for (int i = array.size() - 2; i >= 0; i--)
+			if (comparator.compare(array.get(i), array.get(i + 1)) < 0) {
+				p = i;
+				break;
+			}
+
+		if (p == -1)
+			return false;
+
+		ArrayReverser.reverse(array, p + 1, array.size());
+
+		for (int i = p + 1; i < array.size(); i++)
+			if (comparator.compare(array.get(i), array.get(p)) > 0) {
+				ArraySwapper.swap(array, p, i);
+				break;
+			}
+		return true;
+	}
+
+}
---
+++ src/test/java/org/psjava/algo/NextPermutationTest.java
@@ -0,0 +1,32 @@
+package org.psjava.algo;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.psjava.ds.array.MutableArray;
+import org.psjava.ds.array.MutableArrayFromVarargs;
+import org.psjava.util.DefaultComparator;
+
+import static org.junit.Assert.*;
+
+public class NextPermutationTest {
+
+	@Test
+	public void testStep() {
+		MutableArray<Integer> list = MutableArrayFromVarargs.create(1, 2, 3, 2, 1);
+		boolean success = step(list);
+		assertTrue(success);
+		assertEquals("(1,3,1,2,2)", list.toString());
+	}
+
+	@Test
+	public void testCannotStep() {
+		MutableArray<Integer> list = MutableArrayFromVarargs.create(3, 2, 1);
+		boolean success = step(list);
+		assertFalse(success);
+	}
+
+	private static boolean step(MutableArray<Integer> list) {
+		return NextPermutation.step(list, new DefaultComparator<Integer>());
+	}
+
+}
