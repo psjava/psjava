e423d479fe543f7f2e16781e74f49fadae82c104
kongbomb <kongbomb.coder@gmail.com>
2016-05-12 23:50:09 +0900

    improve one toString for debugging

--- src/main/java/org/psjava/util/FilteredIterable.java
+++ src/main/java/org/psjava/util/FilteredIterable.java
@@ -4,16 +4,16 @@ import java.util.Iterator;
 
 public class FilteredIterable {
 
-	public static <T> Iterable<T> create(final Iterable<? extends T> original, final Filter<T> filter) {
-		return new Iterable<T>() {
-			@Override
-			public Iterator<T> iterator() {
-				return FilteredIterator.create(original.iterator(), filter);
-			}
-		};
-	}
+    public static <T> Iterable<T> create(final Iterable<? extends T> original, final Filter<T> filter) {
+        return IterableUsingIteratorFactory.create(new IteratorFactory<T>() {
+            @Override
+            public Iterator<T> create() {
+                return FilteredIterator.create(original.iterator(), filter);
+            }
+        });
+    }
 
-	private FilteredIterable() {
-	}
+    private FilteredIterable() {
+    }
 
 }
