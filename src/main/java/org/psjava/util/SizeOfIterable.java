b1d8088105af92ae1674ef752da96771af41ad2a
kongbomb <kongbomb.coder@gmail.com>
2016-05-12 16:25:17 +0900

    add a small util - SizeOfIterable

---
+++ src/main/java/org/psjava/util/SizeOfIterable.java
@@ -0,0 +1,12 @@
+package org.psjava.util;
+
+public class SizeOfIterable {
+
+	public static <T> int getSize(Iterable<T> iterable) {
+		int size = 0;
+		for (T t : iterable)
+			size++;
+		return size;
+	}
+
+}
