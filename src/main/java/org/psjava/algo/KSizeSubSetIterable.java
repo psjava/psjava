f9901e35b7ea680e565d88f4b1d715c4134e2c2b
kongbomb <kongbomb.coder@gmail.com>
2016-05-12 16:42:28 +0900

    add an algorithm - KSizeSubSetIterable

---
+++ src/main/java/org/psjava/algo/KSizeSubSetIterable.java
@@ -0,0 +1,33 @@
+package org.psjava.algo;
+
+import org.psjava.ds.array.Array;
+import org.psjava.ds.array.UniformArray;
+import org.psjava.util.*;
+
+public class KSizeSubSetIterable {
+
+	public static <T> Iterable<Iterable<T>> create(final Array<T> superSet, int k) {
+		AssertStatus.assertTrue(k <= superSet.size());
+		Array<Boolean> trues = UniformArray.create(true, k);
+		Array<Boolean> falses = UniformArray.create(false, superSet.size() - k);
+		Iterable<Boolean> flags = MergedIterable.wrap(VarargsIterable.create(trues, falses));
+		Iterable<Array<Boolean>> permutations = PermutationWithRepetitionIterable.create(flags, ReversedComparator.wrap(new DefaultComparator<Boolean>()));
+		return ConvertedIterable.create(permutations, new Converter<Array<Boolean>, Iterable<T>>() {
+			@Override
+			public Iterable<T> convert(final Array<Boolean> permutation) {
+				return ConvertedIterable.create(FilteredIterable.create(ZeroTo.get(permutation.size()), new Filter<Integer>() {
+					@Override
+					public boolean isAccepted(Integer index) {
+						return permutation.get(index);
+					}
+				}), new Converter<Integer, T>() {
+					@Override
+					public T convert(Integer index) {
+						return superSet.get(index);
+					}
+				});
+			}
+		});
+	}
+
+}
---
+++ src/test/java/org/psjava/algo/KSizeSubSetIterableTest.java
@@ -0,0 +1,22 @@
+package org.psjava.algo;
+
+import org.junit.Test;
+import org.psjava.ds.array.ArrayFromVarargs;
+
+import static org.junit.Assert.*;
+
+public class KSizeSubSetIterableTest {
+
+	@Test
+	public void testNormal() {
+		assertEquals("((10,20),(10,30),(20,30))", KSizeSubSetIterable.create(ArrayFromVarargs.create(10, 20, 30), 2).toString());
+		assertEquals("(())", KSizeSubSetIterable.create(ArrayFromVarargs.create(10, 20, 30), 0).toString());
+		assertEquals("((10,20,30))", KSizeSubSetIterable.create(ArrayFromVarargs.create(10, 20, 30), 3).toString());
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testTooBigK() {
+		KSizeSubSetIterable.create(ArrayFromVarargs.create(10, 20, 30), 4);
+	}
+
+}
