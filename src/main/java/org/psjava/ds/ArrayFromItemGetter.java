89cabef452f8d8c969b6fe1b02a55c79834aed49
kongbomb <kongbomb.coder@gmail.com>
2016-05-13 18:18:08 +0900

    add a util to remove duplicated codes - ArrayFromItemGetter

---
+++ src/main/java/org/psjava/ds/ArrayFromItemGetter.java
@@ -0,0 +1,42 @@
+package org.psjava.ds;
+
+import org.psjava.ds.array.Array;
+import org.psjava.ds.array.ArrayIterator;
+import org.psjava.util.GetterByIndex;
+import org.psjava.util.IterableToString;
+
+import java.util.Iterator;
+
+// TODO to make code shorter, try to apply this to exist classes which implements Array
+public class ArrayFromItemGetter {
+
+    public static <T> Array<T> create(final int size, final GetterByIndex<T> getter) {
+        return new Array<T>() {
+            @Override
+            public T get(int index) {
+                return getter.get(index);
+            }
+
+            @Override
+            public int size() {
+                return size;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return size() == 0;
+            }
+
+            @Override
+            public Iterator<T> iterator() {
+                return ArrayIterator.create(this);
+            }
+
+            @Override
+            public String toString() {
+                return IterableToString.toString(this);
+            }
+        };
+    }
+
+}
---
+++ src/main/java/org/psjava/util/GetterByIndex.java
@@ -0,0 +1,5 @@
+package org.psjava.util;
+
+public interface GetterByIndex<T> {
+    T get(int index);
+}
