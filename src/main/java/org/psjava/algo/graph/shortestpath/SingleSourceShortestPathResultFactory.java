3da5d2be81f61b2c0ba8e55be40e44f0245fde82
kongbomb <kongbomb.coder@gmail.com>
2016-06-08 14:35:22 +0900

    improve debug log

--- src/main/java/org/psjava/algo/graph/shortestpath/SingleSourceShortestPathResultFactory.java
+++ src/main/java/org/psjava/algo/graph/shortestpath/SingleSourceShortestPathResultFactory.java
@@ -6,38 +6,44 @@ import org.psjava.ds.graph.DirectedEdge;
 import org.psjava.ds.map.Map;
 import org.psjava.ds.numbersystrem.InfinitableNumber;
 import org.psjava.util.AssertStatus;
+import org.psjava.util.VarargsIterable;
 
 public class SingleSourceShortestPathResultFactory {
 
-	public static <V, E extends DirectedEdge<V>, W> SingleSourceShortestPathResult<V, W, E> create(final V start, final Map<V, InfinitableNumber<W>> distance, final Map<V, E> previous) {
-		return new SingleSourceShortestPathResult<V, W, E>() {
-			@Override
-			public W getDistance(V to) {
-				assertReachable(to);
-				return distance.get(to).getValue();
-			}
+    public static <V, E extends DirectedEdge<V>, W> SingleSourceShortestPathResult<V, W, E> create(final V start, final Map<V, InfinitableNumber<W>> distance, final Map<V, E> previous) {
+        return new SingleSourceShortestPathResult<V, W, E>() {
+            @Override
+            public W getDistance(V to) {
+                assertReachable(to);
+                return distance.get(to).getValue();
+            }
 
-			@Override
-			public Iterable<E> getPath(V to) {
-				assertReachable(to);
-				LinkedList<E> r = new LinkedList<E>();
-				for (V v = to; !v.equals(start); v = previous.get(v).from())
-					r.addFirst(previous.get(v));
-				return r;
-			}
+            @Override
+            public Iterable<E> getPath(V to) {
+                assertReachable(to);
+                LinkedList<E> r = new LinkedList<E>();
+                for (V v = to; !v.equals(start); v = previous.get(v).from())
+                    r.addFirst(previous.get(v));
+                return r;
+            }
 
-			@Override
-			public boolean isReachable(V to) {
-				return !distance.get(to).isInfinity();
-			}
+            @Override
+            public boolean isReachable(V to) {
+                return !distance.get(to).isInfinity();
+            }
 
-			private void assertReachable(V to) {
-				AssertStatus.assertTrue(isReachable(to), "Not reachable");
-			}
-		};
-	}
+            private void assertReachable(V to) {
+                AssertStatus.assertTrue(isReachable(to), "Not reachable");
+            }
 
-	private SingleSourceShortestPathResultFactory() {
-	}
+            @Override
+            public String toString() {
+                return VarargsIterable.create(distance, previous).toString();
+            }
+        };
+    }
+
+    private SingleSourceShortestPathResultFactory() {
+    }
 
 }
