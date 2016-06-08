6c0436cac5455e099380c3f63f5c4f417b033509
kongbomb <kongbomb.coder@gmail.com>
2016-06-08 13:54:53 +0900

    add MapFromKeyValueSequence

---
+++ src/main/java/org/psjava/ds/MapFromKeyValueSequence.java
@@ -0,0 +1,20 @@
+package org.psjava.ds;
+
+import org.psjava.util.AssertStatus;
+import org.psjava.util.IntSequenceIterable;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapFromKeyValueSequence {
+
+    public static <T> Map<T, T> create(List<T> keyValueSequence) {
+        Map<T, T> map = new HashMap<T, T>();
+        AssertStatus.assertTrue(keyValueSequence.size() % 2 == 0);
+        for (int i : IntSequenceIterable.create(0, 2, keyValueSequence.size() / 2))
+            map.put(keyValueSequence.get(i), keyValueSequence.get(i + 1));
+        return map;
+
+    }
+}
