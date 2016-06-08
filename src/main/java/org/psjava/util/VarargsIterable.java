b9dcb05ea12c19c380905809d075830f354a5153
kongbomb <kongbomb.coder@gmail.com>
2016-06-08 13:47:43 +0900

    improve debugging log

--- src/main/java/org/psjava/util/VarargsIterable.java
+++ src/main/java/org/psjava/util/VarargsIterable.java
@@ -4,16 +4,16 @@ import java.util.Iterator;
 
 public class VarargsIterable {
 
-	public static <T> Iterable<T> create(final T... data) {
-		return new Iterable<T>() {
-			@Override
-			public Iterator<T> iterator() {
-				return VarargsIterator.create(data);
-			}
-		};
-	}
+    public static <T> Iterable<T> create(final T... data) {
+        return IterableUsingIteratorFactory.create(new IteratorFactory<T>() {
+            @Override
+            public Iterator<T> create() {
+                return VarargsIterator.create(data);
+            }
+        });
+    }
 
-	private VarargsIterable() {
-	}
+    private VarargsIterable() {
+    }
 
 }
