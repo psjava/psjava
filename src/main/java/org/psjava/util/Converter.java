6e2afb230eb3c12822f30ca91a6900b5d42c7f99
kongbomb <kongbomb.coder@gmail.com>
2016-06-08 14:20:16 +0900

    add comments

--- src/main/java/org/psjava/util/Converter.java
+++ src/main/java/org/psjava/util/Converter.java
@@ -1,5 +1,6 @@
 package org.psjava.util;
 
+// TODO replace to Function?
 public interface Converter<T1, T2> {
 	T2 convert(T1 v);
 }
--- src/main/java/org/psjava/util/Filter.java
+++ src/main/java/org/psjava/util/Filter.java
@@ -1,5 +1,6 @@
 package org.psjava.util;
 
+// TODO rename? Acceptor
 public interface Filter<T> {
 	boolean isAccepted(T v);
 }
