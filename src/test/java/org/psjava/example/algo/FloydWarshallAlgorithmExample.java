67247286a1f10040e67de00c6cd96821f6440640
kongbomb <kongbomb.coder@gmail.com>
2016-07-12 11:59:03 +0900

    introducing weight function on graph - convert to use new floyd warshall

--- src/test/java/org/psjava/example/algo/FloydWarshallAlgorithmExample.java
+++ src/test/java/org/psjava/example/algo/FloydWarshallAlgorithmExample.java
@@ -2,11 +2,11 @@ package org.psjava.example.algo;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.psjava.algo.graph.shortestpath.AllPairShortestPath;
 import org.psjava.algo.graph.shortestpath.AllPairShortestPathResult;
+import org.psjava.algo.graph.shortestpath.AllPairShortestPathV2;
 import org.psjava.algo.graph.shortestpath.FloydWarshallAlgorithm;
-import org.psjava.ds.graph.DirectedWeightedEdge;
-import org.psjava.ds.graph.MutableDirectedWeightedGraph;
+import org.psjava.ds.graph.SimpleDirectedWeightedEdgeV2;
+import org.psjava.ds.graph.SimpleDirectedWeightedGraph;
 import org.psjava.ds.numbersystrem.IntegerNumberSystem;
 
 /**
@@ -15,28 +15,28 @@ import org.psjava.ds.numbersystrem.IntegerNumberSystem;
  */
 public class FloydWarshallAlgorithmExample {
 
-	@Test
-	public void example() {
+    @Test
+    public void example() {
 
-		// Let's construct a graph.
+        // Let's construct a graph.
 
-		MutableDirectedWeightedGraph<String, Integer> graph = MutableDirectedWeightedGraph.create();
-		graph.insertVertex("A");
-		graph.insertVertex("B");
-		graph.insertVertex("C");
-		graph.addEdge("A", "C", 100);
-		graph.addEdge("A", "B", 10);
-		graph.addEdge("B", "C", 20);
+        SimpleDirectedWeightedGraph<String, Integer> graph = SimpleDirectedWeightedGraph.create();
+        graph.insertVertex("A");
+        graph.insertVertex("B");
+        graph.insertVertex("C");
+        graph.addEdge("A", "C", 100);
+        graph.addEdge("A", "B", 10);
+        graph.addEdge("B", "C", 20);
 
-		// Let's get the shortest paths of all pairs.
+        // Let's get the shortest paths of all pairs.
 
-		AllPairShortestPath floyd = FloydWarshallAlgorithm.getInstance();
-		AllPairShortestPathResult<String, Integer, DirectedWeightedEdge<String, Integer>> res = floyd.calc(graph, IntegerNumberSystem.getInstance());
+        AllPairShortestPathV2 floyd = FloydWarshallAlgorithm.INSTANCE;
+        AllPairShortestPathResult<String, Integer, SimpleDirectedWeightedEdgeV2<String, Integer>> res = floyd.calc(graph, graph.getWeightFunction(), IntegerNumberSystem.getInstance());
 
-		int distanceAToB = res.getDistance("A", "B"); // must be 10
-		int distanceBToC = res.getDistance("B", "C"); // must be 20
+        int distanceAToB = res.getDistance("A", "B"); // must be 10
+        int distanceBToC = res.getDistance("B", "C"); // must be 20
 
-		Assert.assertEquals(10, distanceAToB);
-		Assert.assertEquals(20, distanceBToC);
-	}
+        Assert.assertEquals(10, distanceAToB);
+        Assert.assertEquals(20, distanceBToC);
+    }
 }
