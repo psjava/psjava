139688190170b635f480fa0af6344dfba3ebc589
kongbomb <kongbomb.coder@gmail.com>
2016-07-20 12:06:46 +0900

    removing own data structures - removing set - apply to PrimalityTesterBySieve

--- src/main/java/org/psjava/algo/math/numbertheory/PrimalityTesterBySieve.java
+++ src/main/java/org/psjava/algo/math/numbertheory/PrimalityTesterBySieve.java
@@ -1,13 +1,14 @@
 package org.psjava.algo.math.numbertheory;
 
-import org.psjava.ds.set.Set;
-import org.psjava.ds.set.SetFromIterable;
+import org.psjava.ds.SetFromIterableV2;
 import org.psjava.util.AssertStatus;
+
+import java.util.Set;
 
 public class PrimalityTesterBySieve {
 
     public static PrimalityTester newInstance(PrimeNumberSieve sieve, final int max) {
-        final Set<Integer> set = SetFromIterable.create(sieve.calcList(max));
+        final Set<Integer> set = SetFromIterableV2.create(sieve.calcList(max));
         return new PrimalityTester() {
             @Override
             public boolean isPrime(long v) {
@@ -15,9 +16,6 @@ public class PrimalityTesterBySieve {
                 return set.contains((int) v);
             }
         };
-    }
-
-    private PrimalityTesterBySieve() {
     }
 
 }
