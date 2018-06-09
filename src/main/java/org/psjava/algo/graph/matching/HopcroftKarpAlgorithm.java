1ba7bc7efb41eec4c03f870dde9a54b0d2f3ab11
kongbomb <kongbomb.coder@gmail.com>
2018-06-10 02:19:29 +0900

    apply lambda syntax on HopcroftKarpAlgorithm.java

--- src/main/java/org/psjava/algo/graph/matching/HopcroftKarpAlgorithm.java
+++ src/main/java/org/psjava/algo/graph/matching/HopcroftKarpAlgorithm.java
@@ -17,7 +17,6 @@ import org.psjava.ds.graph.DirectedEdge;
 import org.psjava.ds.map.MutableMap;
 import org.psjava.ds.map.ValuesInMap;
 import org.psjava.goods.GoodMutableMapFactory;
-import org.psjava.util.Filter;
 import org.psjava.util.FilteredIterable;
 import org.psjava.util.VisitorStopper;
 import org.psjava.util.ZeroTo;
@@ -108,21 +107,17 @@ public class HopcroftKarpAlgorithm {
     private static <V> Collection<Vertex<V>> bfs(final Graph<Vertex<V>, Edge<V>> adj, final Object mark) {
         final DynamicArray<Vertex<V>> finishes = DynamicArray.create();
 
-        BFS.traverse(EdgeFilteredSubGraph.wrap(adj, new Filter<Edge<V>>() {
-            @Override
-            public boolean isAccepted(Edge<V> edge) {
-                // to alternate matched and non-matched edges.
-                if (edge.from.side == Side.LEFT)
-                    return !edge.status.inMatch;
-                else
-                    return edge.status.inMatch;
-            }
-        }), FilteredIterable.create(adj.getVertices(), new Filter<Vertex<V>>() {
-            @Override
-            public boolean isAccepted(Vertex<V> v) {
-                return (v.side == Side.LEFT) && v.free;
-            }
-        }), new BFSVisitor<Vertex<V>, Edge<V>>() {
+        Graph<Vertex<V>, Edge<V>> subGraph = EdgeFilteredSubGraph.wrap(adj, edge -> {
+            // to alternate matched and non-matched edges.
+            if (edge.from.side == Side.LEFT)
+                return !edge.status.inMatch;
+            else
+                return edge.status.inMatch;
+        });
+
+        Iterable<Vertex<V>> freeLeftVertexes = FilteredIterable.create(adj.getVertices(), v -> (v.side == Side.LEFT) && v.free);
+
+        BFS.traverse(subGraph, freeLeftVertexes, new BFSVisitor<Vertex<V>, Edge<V>>() {
             int finishDepth = -1;
 
             @Override
@@ -147,12 +142,10 @@ public class HopcroftKarpAlgorithm {
     }
 
     private static <V> void dfs(final Graph<Vertex<V>, Edge<V>> adj, final Collection<Vertex<V>> bfsFinishes, final Object bfsMark) {
-        MultiSourceDFS.traverse(EdgeFilteredSubGraph.wrap(adj, new Filter<Edge<V>>() {
-            @Override
-            public boolean isAccepted(Edge<V> edge) {
-                return edge.status.bfsMark == bfsMark; // uses only edges discovered in bfs step.
-            }
-        }), bfsFinishes, new DFSVisitorBase<Vertex<V>, Edge<V>>() {
+        // uses only edges discovered in bfs step.
+        Graph<Vertex<V>, Edge<V>> subGraph = EdgeFilteredSubGraph.wrap(adj, edge -> edge.status.bfsMark == bfsMark);
+
+        MultiSourceDFS.traverse(subGraph, bfsFinishes, new DFSVisitorBase<Vertex<V>, Edge<V>>() {
             DynamicArray<Edge<V>> path = DynamicArray.create();
 
             @Override
