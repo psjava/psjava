1ba249b5311c0e3274ce762a3494a7680510d3ba
kongbomb <kongbomb.coder@gmail.com>
2018-06-10 23:17:24 +0900

    use new MapFactory in DFSStatusMap

--- src/main/java/org/psjava/DFSStatusMap.java
+++ src/main/java/org/psjava/DFSStatusMap.java
@@ -1,24 +1,19 @@
 package org.psjava;
 
 import org.psjava.algo.graph.dfs.DFSStatus;
-import org.psjava.ds.map.MutableMap;
-import org.psjava.goods.GoodMutableMapFactory;
+
+import java.util.Map;
 
 public class DFSStatusMap<V> {
 
-    private MutableMap<V, DFSStatus> map = GoodMutableMapFactory.getInstance().create(); // TODO to Map
+    private Map<V, DFSStatus> map = HashMapFactory.<V>create().create();
 
     public DFSStatus get(V v) {
-        DFSStatus dfsStatus = map.getOrNull(v);
-        if (dfsStatus != null) {
-            return dfsStatus;
-        } else {
-            return DFSStatus.NOT_DISCOVERED;
-        }
+        return map.getOrDefault(v, DFSStatus.NOT_DISCOVERED);
     }
 
     public void set(V v, DFSStatus s) {
-        map.addOrReplace(v, s);
+        map.put(v, s);
     }
 
 }
