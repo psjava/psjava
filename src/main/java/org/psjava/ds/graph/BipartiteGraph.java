06f2c43197fff431823c01c1d3b6fa85ec058905
kongbomb <kongbomb.coder@gmail.com>
2018-06-18 13:44:37 +0900

    remove using own data structure in BipartiteGraph

--- src/main/java/org/psjava/ds/graph/BipartiteGraph.java
+++ src/main/java/org/psjava/ds/graph/BipartiteGraph.java
@@ -1,52 +1,49 @@
 package org.psjava.ds.graph;
 
-import org.psjava.ds.PSCollection;
-import org.psjava.ds.array.DynamicArray;
-import org.psjava.ds.set.MutableSet;
-import org.psjava.goods.GoodMutableSetFactory;
 import org.psjava.util.AssertStatus;
-import org.psjava.util.IterableToString;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class BipartiteGraph<V> {
 
-    private MutableSet<V> left = GoodMutableSetFactory.getInstance().create();
-    private MutableSet<V> right = GoodMutableSetFactory.getInstance().create();
-    private DynamicArray<BipartiteGraphEdge<V>> edges = DynamicArray.create();
+    private Set<V> lefts = new HashSet<>();
+    private Set<V> rights = new HashSet<>();
+    private List<BipartiteGraphEdge<V>> edges = new ArrayList<>();
 
     public void insertLeftVertex(V v) {
-        AssertStatus.assertTrue(!right.contains(v));
-        left.addIfAbsent(v);
+        AssertStatus.assertTrue(!rights.contains(v));
+        lefts.add(v);
     }
 
     public void insertRightVertex(V v) {
-        AssertStatus.assertTrue(!left.contains(v));
-        right.addIfAbsent(v);
+        AssertStatus.assertTrue(!lefts.contains(v));
+        rights.add(v);
     }
 
-    public void addEdge(final V leftv, final V rightv) {
-        assertVertexExist(leftv, left);
-        assertVertexExist(rightv, right);
-        edges.addToLast(SimpleBipartiteGraphEdge.create(leftv, rightv));
+    public void addEdge(final V left, final V right) {
+        assertVertexExist(left, lefts);
+        assertVertexExist(right, rights);
+        edges.add(SimpleBipartiteGraphEdge.create(left, right));
     }
 
-    private void assertVertexExist(V v, MutableSet<V> set) {
+    private void assertVertexExist(V v, Set<V> set) {
         AssertStatus.assertTrue(set.contains(v), "vertex is not in graph");
     }
 
-    public PSCollection<V> getLeftVertices() {
-        return left;
+    public Collection<V> getLeftVertices() {
+        return Collections.unmodifiableCollection(lefts);
     }
 
-    public PSCollection<V> getRightVertices() {
-        return right;
+    public Collection<V> getRightVertices() {
+        return Collections.unmodifiableCollection(rights);
     }
 
-    public Iterable<BipartiteGraphEdge<V>> getEdges() {
-        return edges;
-    }
-
-    @Override
-    public String toString() {
-        return this.getClass().getSimpleName() + ":" + IterableToString.toString(getEdges());
+    public Collection<BipartiteGraphEdge<V>> getEdges() {
+        return Collections.unmodifiableCollection(edges);
     }
 }
