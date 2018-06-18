13cbeddb248dd4f9553d625d8e06aedeb8e0501d
kongbomb <kongbomb.coder@gmail.com>
2018-06-18 16:09:23 +0900

    remove useless algorithm - MaximumBipartiteMatchingAlgorithmUsingMaximumFlow. because it's slow.

--- src/main/java/org/psjava/algo/graph/matching/MaximumBipartiteMatchingAlgorithm.java
+++
@@ -1,8 +0,0 @@
-package org.psjava.algo.graph.matching;
-
-import org.psjava.BipartiteGraph;
-
-@Deprecated
-public interface MaximumBipartiteMatchingAlgorithm {
-    <V> int calc(BipartiteGraph<V> graph);
-}
