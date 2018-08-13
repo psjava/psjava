749b3ad0671f89ca02dd4db0c4af5a57dafbb724
kongbomb <kongbomb.coder@gmail.com>
2018-08-13 23:26:28 +0900

    use java 8 syntax - SegmentTreeFactoryByArrayImplementation

--- src/main/java/org/psjava/ds/tree/segmenttree/SegmentTreeFactoryByArrayImplementation.java
+++ src/main/java/org/psjava/ds/tree/segmenttree/SegmentTreeFactoryByArrayImplementation.java
@@ -1,20 +1,9 @@
 package org.psjava.ds.tree.segmenttree;
 
-import org.psjava.ds.array.PSArray;
-import org.psjava.ds.math.BinaryOperator;
-
 public class SegmentTreeFactoryByArrayImplementation {
 
     public static SegmentTreeFactory getInstance() {
-        return new SegmentTreeFactory() {
-            @Override
-            public <T> SegmentTree<T> create(PSArray<T> initialList, BinaryOperator<T> operator) {
-                return new SegmentTreeByArrayImplementation<T>(initialList, operator);
-            }
-        };
-    }
-
-    private SegmentTreeFactoryByArrayImplementation() {
+        return SegmentTreeByArrayImplementation::new;
     }
 
 }
