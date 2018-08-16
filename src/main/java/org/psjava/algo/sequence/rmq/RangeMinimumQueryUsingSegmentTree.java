de4a50d3fc2c38e84010af4fe0f39de7b7a01fa0
kongbomb <kongbomb.coder@gmail.com>
2018-08-16 15:44:07 +0900

    change deprecated usage - SegmentTreeByArrayImplementation() in RangeMinimumQueryUsingSegmentTree

--- src/main/java/org/psjava/algo/sequence/rmq/RangeMinimumQueryUsingSegmentTree.java
+++ src/main/java/org/psjava/algo/sequence/rmq/RangeMinimumQueryUsingSegmentTree.java
@@ -1,14 +1,14 @@
 package org.psjava.algo.sequence.rmq;
 
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.psjava.ds.array.PSArray;
-import org.psjava.ds.array.MutableArray;
-import org.psjava.ds.array.MutableArrayFactory;
 import org.psjava.ds.tree.segmenttree.SegmentTree;
 import org.psjava.ds.tree.segmenttree.SegmentTreeByArrayImplementation;
 import org.psjava.util.Assertion;
-import org.psjava.util.ZeroTo;
 
 /**
  * Time complexity for preprocessing: Time complexity for constructing segment tree. known best is, O(n)
@@ -22,10 +22,9 @@ public class RangeMinimumQueryUsingSegmentTree {
     public static final RangeMinimumQuery INSTANCE = new RangeMinimumQuery() {
         @Override
         public <T> RangeMinimumQuerySession preprocess(final PSArray<T> a, final Comparator<T> comp) {
-            MutableArray<Integer> indexes = MutableArrayFactory.create(a.size(), 0);
-            for (int i : ZeroTo.get(a.size()))
-                indexes.set(i, i);
-            final SegmentTree<Integer> tree = new SegmentTreeByArrayImplementation<>(indexes, (i1, i2) -> RangeMinimumQueryUtil.selectSmallestIndex(a, i1, i2, comp));
+            List<Integer> init = IntStream.range(0, a.size())
+                    .boxed().collect(Collectors.toList());
+            final SegmentTree<Integer> tree = new SegmentTreeByArrayImplementation<>(init, (i1, i2) -> RangeMinimumQueryUtil.selectSmallestIndex(a, i1, i2, comp));
             return (start, end) -> {
                 Assertion.ensure(start < end);
                 return tree.query(start, end);
