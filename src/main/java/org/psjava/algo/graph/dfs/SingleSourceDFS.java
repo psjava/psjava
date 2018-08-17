ed23d5bbc81d9375d23ac7442e742b430cdbbfdf
kongbomb <kongbomb.coder@gmail.com>
2018-08-17 23:26:41 +0900

    apply new graph ds to SingleSourceDFS.traverse()

--- src/main/java/org/psjava/algo/graph/dfs/SingleSourceDFS.java
+++ src/main/java/org/psjava/algo/graph/dfs/SingleSourceDFS.java
@@ -1,24 +1,42 @@
 package org.psjava.algo.graph.dfs;
 
-import org.psjava.AdjacencyList;
-import org.psjava.ds.graph.Graph;
+import org.psjava.GraphV2;
+import org.psjava.UnweightedGraph;
 import org.psjava.ds.graph.DirectedEdge;
+import org.psjava.ds.graph.Graph;
+import org.psjava.util.VisitorStopper;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Function;
+import java.util.function.Consumer;
 
 public class SingleSourceDFS {
 
+    // TODO inline
     @Deprecated
     public static <V, E extends DirectedEdge<V>> void traverse(Graph<V, E> adj, V start, DFSVisitor<V, E> visitor) {
-        traverse(adj::getEdges, DirectedEdge::to, start, visitor);
+        Map<V, DFSStatus> map = new HashMap<>();
+        DFSCore.traverse(start, adj::getEdges, DirectedEdge::to, map::get, map::put, visitor);
     }
 
-    @Deprecated
-    public static <V, E> void traverse(AdjacencyList<V, E> adjacencyList, Function<E, V> destination, V start, DFSVisitor<V, E> visitor) {
+    // TODO make public manual
+    public static <V> void traverse(UnweightedGraph<V> unweightedGraph, V start, final Consumer<V> onDiscover, final Consumer<V> onFinish) {
+        traverse(unweightedGraph.graph, start, onDiscover, onFinish);
+    }
+
+    private static <V> void traverse(GraphV2<V, V> graph, V start, Consumer<V> onDiscover, Consumer<V> onFinish) {
         Map<V, DFSStatus> map = new HashMap<>();
-        DFSCore.traverse(start, adjacencyList, destination, map::get, map::put, visitor);
+        DFSCore.traverse(start, v -> graph.edgeMap.get(v), v -> v, map::get, map::put, new DFSVisitorBase<V, V>() {
+            @Override
+            public void onDiscovered(V vertex, int depth, VisitorStopper stopper) {
+                onDiscover.accept(vertex);
+            }
+
+            @Override
+            public void onFinish(V vertex) {
+                onFinish.accept(vertex);
+            }
+        });
     }
 
 }
---
+++ src/test/java/org/psjava/example/SingleSourceDFSExample.java
@@ -0,0 +1,33 @@
+package org.psjava.example;
+
+import org.psjava.UnweightedGraph;
+import org.psjava.algo.graph.dfs.SingleSourceDFS;
+
+public class SingleSourceDFSExample {
+
+    public static void main(String[] args) {
+
+        // Let's prepare a simple graph.
+
+        UnweightedGraph<String> graph = new UnweightedGraph<>();
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addVertex("D");
+
+        graph.addEdge("A", "B");
+        graph.addEdge("B", "C");
+        graph.addEdge("C", "D");
+        graph.addEdge("C", "A");
+
+        // traverse and handle searching events.
+
+        SingleSourceDFS.traverse(
+                graph,
+                "A",
+                vertex -> System.out.println("discover " + vertex),
+                vertex -> System.out.println("finish " + vertex)
+        );
+
+    }
+}
